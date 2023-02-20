62fae2d581b0b1f705ba1335ffaeead9be093c70
Bim Overbohm <bim.overbohm@googlemail.com>
2023-02-20 16:05:36 +0100

    Add shared_ptr type to statistics window

--- src/statistics/statistics_window.h
+++ src/statistics/statistics_window.h
@@ -3,12 +3,16 @@
 #include "statistics.h"
 #include "ui/gui_sdl.h"
 
+#include <memory>
+
 namespace Statistics
 {
 
     class Window : public Ui::SDLWindow
     {
     public:
+        using SPtr = std::shared_ptr<Window>;
+
         Window(uint32_t width, uint32_t height);
 
         auto getStatisticsContainer() -> Container::SPtr;
