b43b5756962bedaf3d76b8cd2b21b243876e6ab4
Bim Overbohm <bim.overbohm@googlemail.com>
2023-02-23 23:55:24 +0100

    Add XRGB888 color

---
+++ src/color/xrgb888.cpp
@@ -0,0 +1,21 @@
+#include "xrgb888.h"
+
+namespace Color
+{
+
+    auto XRGB888::distance(const XRGB888 &color0, const XRGB888 &color1) -> float
+    {
+        if (color0.c == color1.c)
+        {
+            return 0.0F;
+        }
+        float ra = static_cast<float>(color0.R()) / 255.0F;
+        float rb = static_cast<float>(color1.R()) / 255.0F;
+        float rMean = 0.5F * (ra + rb);
+        float dR = ra - rb;
+        float dG = static_cast<float>(color0.G()) - static_cast<float>(color1.G());
+        float dB = static_cast<float>(color0.B()) - static_cast<float>(color1.B());
+        return ((2.0F + rMean) * dR * dR + 4.0F * dG * dG + (3.0F - rMean) * dB * dB) / 9.0F;
+    } // max:   (2    +   0.5) *  1 *  1 + 4    *  1 *  1 + (3 -      0.5) *  1 *  1 = 2.5 + 4 + 2.5 = 9 / 9 = 1
+
+}
---
+++ src/color/xrgb888.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Color
+{
+
+    /// @brief XRGB888 color in range [0,255]
+    class XRGB888
+    {
+    public:
+        XRGB888() = default;
+        constexpr XRGB888(uint8_t R, uint8_t G, uint8_t B) : r(R), g(G), b(B) {}
+        constexpr XRGB888(uint32_t color) : c(color) {}
+
+        inline XRGB888 &operator=(const XRGB888 &other)
+        {
+            c = other.c;
+            return *this;
+        }
+
+        inline XRGB888 &operator=(uint32_t color)
+        {
+            c = color;
+            return *this;
+        }
+
+        inline auto R() const -> const uint8_t { return r; }
+        inline auto R() -> uint8_t { return r; }
+        inline auto G() const -> const uint8_t { return g; }
+        inline auto G() -> uint8_t { return g; }
+        inline auto B() const -> const uint8_t { return b; }
+        inline auto B() -> uint8_t { return b; }
+
+        inline operator uint32_t() const { return c; }
+
+        /// @brief Calculate square of perceived distance between colors
+        /// See: https://stackoverflow.com/a/40950076 and https://www.compuphase.com/cmetric.htm
+        /// @return Returns a value in [0,1]
+        static auto distance(const XRGB888 &color0, const XRGB888 &color1) -> float;
+
+    private:
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+        union
+        {
+            struct
+            {
+                uint8_t b;
+                uint8_t g;
+                uint8_t r;
+                uint8_t x;
+            } __attribute__((aligned(4), packed));
+            uint32_t c = 0;
+        } __attribute__((aligned(4), packed));
+#pragma GCC diagnostic pop
+    };
+
+}
