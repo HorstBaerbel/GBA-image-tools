9dfaace9f1da41fe028e0f83a50b30535e613a5f
Bim Overbohm <bim.overbohm@googlemail.com>
2023-02-26 15:30:00 +0100

    Fix XRGB888 distance calculation

--- src/color/xrgb888.cpp
+++ src/color/xrgb888.cpp
@@ -13,8 +13,8 @@ namespace Color
         float rb = static_cast<float>(color1.R()) / 255.0F;
         float rMean = 0.5F * (ra + rb);
         float dR = ra - rb;
-        float dG = static_cast<float>(color0.G()) - static_cast<float>(color1.G());
-        float dB = static_cast<float>(color0.B()) - static_cast<float>(color1.B());
+        float dG = static_cast<float>(color0.G()) - static_cast<float>(color1.G()) / 255.0F;
+        float dB = static_cast<float>(color0.B()) - static_cast<float>(color1.B()) / 255.0F;
         return ((2.0F + rMean) * dR * dR + 4.0F * dG * dG + (3.0F - rMean) * dB * dB) / 9.0F;
     } // max:   (2    +   0.5) *  1 *  1 + 4    *  1 *  1 + (3 -      0.5) *  1 *  1 = 2.5 + 4 + 2.5 = 9 / 9 = 1
 
