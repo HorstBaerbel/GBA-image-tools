4781470fe7d34d7c9542d215ea46871bce9f49b8
Bim Overbohm <bim.overbohm@googlemail.com>
2023-06-05 23:12:22 +0200

    Refactor sprite helpers

--- src/processing/spritehelpers.cpp
+++ src/processing/spritehelpers.cpp
@@ -2,146 +2,118 @@
 
 #include "exception.h"
 
+#include <algorithm>
 #include <cstring>
 #include <map>
 #include <array>
 
-std::vector<uint8_t> convertToWidth(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t tileWidth)
+auto convertToWidth(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth) -> std::vector<Color::XRGB8888>
 {
-    bitsPerPixel = bitsPerPixel == 15 ? 16 : bitsPerPixel;
-    REQUIRE(bitsPerPixel == 4 || bitsPerPixel == 8 || bitsPerPixel == 16, std::runtime_error, "Bits per pixel must be in [4, 8, 15, 16]");
+    REQUIRE(data.size() == width * height, std::runtime_error, "Data size must be == width * height");
     REQUIRE(tileWidth % 8 == 0, std::runtime_error, "Tile width must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
-    std::vector<uint8_t> dst(src.size());
-    const uint32_t bytesPerTileLine = bitsPerPixel * (tileWidth / 8);
-    const uint32_t bytesPerSrcLine = (width * bitsPerPixel) / 8;
-    uint8_t *dstData = dst.data();
+    std::vector<Color::XRGB8888> dst(data.size());
+    auto dstData = dst.begin();
     for (uint32_t blockX = 0; blockX < width; blockX += tileWidth)
     {
-        const uint8_t *srcLine = src.data() + (blockX * bitsPerPixel) / 8;
+        auto srcLine = std::next(data.cbegin(), blockX);
         for (uint32_t tileY = 0; tileY < height; ++tileY)
         {
-            std::memcpy(dstData, srcLine, bytesPerTileLine);
-            dstData += bytesPerTileLine;
-            srcLine += bytesPerSrcLine;
+            std::copy_n(srcLine, tileWidth, dstData);
+            dstData = std::next(dstData, tileWidth);
+            srcLine = std::next(srcLine, width);
         }
     }
     return dst;
 }
 
-std::vector<uint8_t> convertToTiles(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t tileWidth, uint32_t tileHeight)
+auto convertToTiles(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth, uint32_t tileHeight) -> std::vector<Color::XRGB8888>
 {
-    bitsPerPixel = bitsPerPixel == 15 ? 16 : bitsPerPixel;
-    REQUIRE(bitsPerPixel == 4 || bitsPerPixel == 8 || bitsPerPixel == 16, std::runtime_error, "Bits per pixel must be in [4, 8, 15, 16]");
+    REQUIRE(data.size() == width * height, std::runtime_error, "Data size must be == width * height");
     REQUIRE(tileWidth % 8 == 0 && tileHeight % 8 == 0, std::runtime_error, "Tile width and height must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
-    std::vector<uint8_t> dst(src.size());
-    const uint32_t bytesPerTileLine = bitsPerPixel * (tileWidth / 8);
-    const uint32_t bytesPerSrcLine = (width * bitsPerPixel) / 8;
-    uint8_t *dstData = dst.data();
+    std::vector<Color::XRGB8888> dst(data.size());
+    auto dstData = dst.begin();
     for (uint32_t blockY = 0; blockY < height; blockY += tileHeight)
     {
-        const uint8_t *srcBlock = src.data() + blockY * bytesPerSrcLine;
+        auto srcBlock = std::next(data.cbegin(), blockY * width);
         for (uint32_t blockX = 0; blockX < width; blockX += tileWidth)
         {
-            const uint8_t *srcLine = srcBlock + (blockX * bitsPerPixel) / 8;
+            auto srcLine = std::next(srcBlock, blockX);
             for (uint32_t tileY = 0; tileY < tileHeight; ++tileY)
             {
-                std::memcpy(dstData, srcLine, bytesPerTileLine);
-                dstData += bytesPerTileLine;
-                srcLine += bytesPerSrcLine;
+                std::copy_n(srcLine, tileWidth, dstData);
+                dstData = std::next(dstData, tileWidth);
+                srcLine = std::next(srcLine, width);
             }
         }
     }
     return dst;
 }
 
-std::vector<uint8_t> convertToSprites(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t spriteWidth, uint32_t spriteHeight)
+auto convertToSprites(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>
 {
-    bitsPerPixel = bitsPerPixel == 15 ? 16 : bitsPerPixel;
-    REQUIRE(bitsPerPixel == 4 || bitsPerPixel == 8 || bitsPerPixel == 16, std::runtime_error, "Bits per pixel must be in [4, 8, 15, 16]");
     REQUIRE(spriteWidth % 8 == 0 && spriteHeight % 8 == 0, std::runtime_error, "Sprite width and height must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
-    // convert to tiles first
-    auto tileData = convertToTiles(src, width, height, bitsPerPixel);
-    // now convert to sprites
-    std::vector<uint8_t> dst(src.size());
-    const uint32_t bytesPerTile = bitsPerPixel * 8;
-    const uint32_t bytesPerSrcLine = (width * bitsPerPixel) / 8;
-    const uint32_t spritesHorizontal = width / spriteWidth;
-    const uint32_t spritesVertical = height / spriteHeight;
-    const uint32_t spriteTileWidth = spriteWidth / 8;
-    const uint32_t spriteTileHeight = spriteHeight / 8;
-    const uint32_t bytesPerSpriteLine = spriteTileWidth * bytesPerTile;
-    uint8_t *dstData = dst.data();
-    for (uint32_t spriteY = 0; spriteY < spritesVertical; ++spriteY)
-    {
-        const uint8_t *srcBlock = src.data() + spriteY * spriteHeight * bytesPerSrcLine;
-        for (uint32_t spriteX = 0; spriteX < spritesHorizontal; ++spriteX)
-        {
-            const uint8_t *srcTile = srcBlock + spriteX * bytesPerSpriteLine;
-            for (uint32_t tileY = 0; tileY < spriteTileHeight; ++tileY)
-            {
-                std::memcpy(dstData, srcTile, bytesPerSpriteLine);
-                dstData += bytesPerSpriteLine;
-                srcTile += bytesPerSrcLine * 8;
-            }
-        }
-    }
-    return dst;
+    return convertToTiles(convertToWidth(src, width, height, spriteWidth), width, height);
 }
 
 // Hash tile block 4 different ways: normal, flipped horizontally, flipped vertically, flipped in both directions
 // Uses modified DJB2 hash functions. Not terribly efficient
-std::array<uint32_t, 4> hashTileBlock(std::vector<uint8_t>::const_iterator src, uint32_t columns, uint32_t rows, uint32_t bytesPerRow)
+auto hashTileBlock(std::vector<Color::XRGB8888>::const_iterator src, uint32_t columns, uint32_t rows, uint32_t pixelsPerRow) -> std::array<uint32_t, 4>
 {
     std::array<uint32_t, 4> hash = {15359, 11447, 6991, 5381};
     // hash normally
     auto src0 = src;
-    for (int32_t i = 0; i < (columns * rows); i++)
+    for (int32_t i = 0; i < (columns * rows); i++, ++src0)
     {
-        hash[0] = hash[0] * 53 + *src0++;
+        hash[0] = hash[0] * (11 + static_cast<uint32_t>(src0->R()));
+        hash[0] = hash[0] * (31 + static_cast<uint32_t>(src0->G()));
+        hash[0] = hash[0] * (53 + static_cast<uint32_t>(src0->B()));
     }
     // hash flipped horizontally
     for (int32_t y = 0; y < rows; y++)
     {
-        auto src1 = std::next(src, y * bytesPerRow + bytesPerRow - 1);
-        for (int32_t x = 0; x < columns; x++)
+        auto src1 = std::next(src, y * pixelsPerRow + pixelsPerRow - 1);
+        for (int32_t x = 0; x < columns; x++, --src1)
         {
-            hash[1] = hash[1] * 43 + *src1--;
+            hash[1] = hash[1] * (7 + static_cast<uint32_t>(src1->R()));
+            hash[1] = hash[1] * (61 + static_cast<uint32_t>(src1->G()));
+            hash[1] = hash[1] * (43 + static_cast<uint32_t>(src1->B()));
         }
     }
     // hash flipped vertically
     for (int32_t y = rows - 1; y >= 0; y--)
     {
-        auto src2 = std::next(src, y * bytesPerRow);
-        for (int32_t x = 0; x < columns; x++)
+        auto src2 = std::next(src, y * pixelsPerRow);
+        for (int32_t x = 0; x < columns; x++, ++src2)
         {
-            hash[2] = hash[2] * 37 + *src2++;
+            hash[2] = hash[2] * (47 + static_cast<uint32_t>(src2->R()));
+            hash[2] = hash[2] * (19 + static_cast<uint32_t>(src2->G()));
+            hash[2] = hash[2] * (37 + static_cast<uint32_t>(src2->B()));
         }
     }
     // hash flipped both ways
     for (int32_t y = rows - 1; y >= 0; y--)
     {
-        auto src3 = std::next(src, y * bytesPerRow + bytesPerRow - 1);
-        for (int32_t x = 0; x < columns; x++)
+        auto src3 = std::next(src, y * pixelsPerRow + pixelsPerRow - 1);
+        for (int32_t x = 0; x < columns; x++, --src3)
         {
-            hash[3] = hash[3] * 29 + *src3--;
+            hash[3] = hash[3] * (29 + static_cast<uint32_t>(src3->R()));
+            hash[3] = hash[3] * (17 + static_cast<uint32_t>(src3->G()));
+            hash[3] = hash[3] * (13 + static_cast<uint32_t>(src3->B()));
         }
     }
     return hash;
 }
 
-std::pair<std::vector<uint16_t>, std::vector<uint8_t>> buildUniqueTileMap(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, bool detectFlips, uint32_t tileWidth, uint32_t tileHeight)
+auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth, uint32_t tileHeight) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>
 {
-    bitsPerPixel = bitsPerPixel == 15 ? 16 : bitsPerPixel;
-    REQUIRE(bitsPerPixel == 4 || bitsPerPixel == 8 || bitsPerPixel == 16, std::runtime_error, "Bits per pixel must be in [4, 8, 15, 16]");
     REQUIRE(tileWidth % 8 == 0 && tileHeight % 8 == 0, std::runtime_error, "Tile width and height must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
-    const uint32_t bytesPerTileRow = bitsPerPixel;
-    const uint32_t bytesPerTile = 8 * bitsPerPixel * (tileWidth / 8);
+    const uint32_t pixelsPerTile = tileWidth * tileHeight;
     std::vector<uint16_t> dstScreen(width / tileWidth * height / tileHeight); // screen map
-    std::vector<uint8_t> dstTiles;                                            // unique tile map
+    std::vector<Color::XRGB8888> dstTiles;                                    // unique tile map
     uint16_t nrOfUniqueTiles = 0;                                             // # of tiles currently in tile map
     std::map<uint32_t, uint16_t> dstTileHashes;                               // map from hash values of tile pixels -> tile map index
     std::map<uint32_t, uint16_t> dstTileHashesH;                              // map from hash values of tile pixels swapped in H -> tile map index
@@ -152,7 +124,7 @@ std::pair<std::vector<uint16_t>, std::vector<uint8_t>> buildUniqueTileMap(const 
     for (uint32_t tileIndex = 0; tileIndex < dstScreen.size(); tileIndex++)
     {
         // hash tile pixel block
-        auto tileHash = hashTileBlock(srcIt, tileWidth, tileHeight, bytesPerTileRow);
+        auto tileHash = hashTileBlock(srcIt, tileWidth, tileHeight, width);
         // check if tile pixel hash can be found in one of the hash maps
         if (auto tileIt = dstTileHashes.find(tileHash[0]); tileIt != dstTileHashes.cend())
         {
@@ -174,16 +146,17 @@ std::pair<std::vector<uint16_t>, std::vector<uint8_t>> buildUniqueTileMap(const 
         {
             REQUIRE(nrOfUniqueTiles < 1024, std::runtime_error, "Too many unique tiles. Max 1023 tiles allowed");
             // tile not in map. add new tile index
+            dstScreen[tileIndex] = nrOfUniqueTiles;
+            // add this tiles index to all hash maps
             dstTileHashes[tileHash[0]] = nrOfUniqueTiles;
             dstTileHashesH[tileHash[1]] = nrOfUniqueTiles | (1 << 10);
             dstTileHashesV[tileHash[2]] = nrOfUniqueTiles | (1 << 11);
             dstTileHashesHV[tileHash[3]] = nrOfUniqueTiles | (3 << 10);
-            dstScreen[tileIndex] = nrOfUniqueTiles;
             nrOfUniqueTiles++;
             // copy new tile data to tile map
-            std::copy(srcIt, std::next(srcIt, bytesPerTile), std::back_inserter(dstTiles));
+            std::copy(srcIt, std::next(srcIt, pixelsPerTile), std::back_inserter(dstTiles));
         }
-        srcIt = std::next(srcIt, bytesPerTile);
+        srcIt = std::next(srcIt, pixelsPerTile);
     }
-    return {dstScreen, dstTiles};
+    return std::make_pair(dstScreen, dstTiles);
 }
--- src/processing/spritehelpers.h
+++ src/processing/spritehelpers.h
@@ -1,22 +1,23 @@
-// some image utility functions used by the tools
 #pragma once
+
+#include "color/xrgb8888.h"
 
 #include <cstdint>
 #include <vector>
 
 /// @brief Cut data to tileWidth * height pixel wide tiles. Width and height and tileWidth MUST be a multiple of 8!
-std::vector<uint8_t> convertToWidth(const std::vector<uint8_t> &data, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t tileWidth);
+auto convertToWidth(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth) -> std::vector<Color::XRGB8888>;
 
 /// @brief Cut data to 8x8 pixel wide tiles and store per tile instead of per scanline.
 /// Width and height MUST be a multiple of 8!
-std::vector<uint8_t> convertToTiles(const std::vector<uint8_t> &data, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t tileWidth = 8, uint32_t tileHeight = 8);
+auto convertToTiles(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth = 8, uint32_t tileHeight = 8) -> std::vector<Color::XRGB8888>;
 
 /// @brief Cut data to 8x8 pixel wide tiles and store per sprite instead of per scanline.
 /// Width and height MUST be a multiple of 8 and of spriteWidth and spriteHeight.
-std::vector<uint8_t> convertToSprites(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, uint32_t spriteWidth, uint32_t spriteHeight);
+auto convertToSprites(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>;
 
 /// @brief Build a screen and tile map from tile data, storing only unique tiles. Only max. 1024 unique tiles allowed!
 /// Source data MUST have been converted to tiles already and width and height MUST be a multiple of 8!
 /// Will detect horizontally, vertically and horizontally+vertically flipped tiles and will set the map index flip flags accordingly (if detectFlips == true)
 /// @return Returns (screen map, unique tile map)
-std::pair<std::vector<uint16_t>, std::vector<uint8_t>> buildUniqueTileMap(const std::vector<uint8_t> &src, uint32_t width, uint32_t height, uint32_t bitsPerPixel, bool detectFlips, uint32_t tileWidth = 8, uint32_t tileHeight = 8);
+auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth = 8, uint32_t tileHeight = 8) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>;
