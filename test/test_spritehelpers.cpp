68cf8a1808bbd5505aad1c1f871ad72c87c483e3
Bim Overbohm <bim.overbohm@googlemail.com>
2023-06-24 18:53:01 +0200

    Add tests for buildUniqueTileMap

--- test/test_spritehelpers.cpp
+++ test/test_spritehelpers.cpp
@@ -31,10 +31,10 @@ CATCH_TEST_CASE("convertToWidth", TEST_SUITE_TAG)
     // ...
     std::span s1(v1);
     std::span s2(v2);
-    for (std::size_t i = 0; i < 56; i += 8)
+    for (std::size_t i = 0; i < 64; i += 8)
     {
-        CATCH_REQUIRE_THAT(s2.subspan(i, 8), Catch::Matchers::RangeEquals(s1.subspan(2 * i, 8)));
-        CATCH_REQUIRE_THAT(s2.subspan(i + 64, 8), Catch::Matchers::RangeEquals(s1.subspan(2 * i + 8, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 0 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 1 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i + 8, 8)));
     }
     CATCH_REQUIRE(v2 == convertToTiles(v1, 16, 8, 8, 8));
 }
@@ -61,10 +61,12 @@ CATCH_TEST_CASE("convertToTiles", TEST_SUITE_TAG)
     // ...
     std::span s1(v1);
     std::span s2(v2);
-    for (std::size_t i = 0; i < 112; i += 8)
+    for (std::size_t i = 0; i < 64; i += 8)
     {
-        CATCH_REQUIRE_THAT(s2.subspan(i, 8), Catch::Matchers::RangeEquals(s1.subspan(2 * i, 8)));
-        CATCH_REQUIRE_THAT(s2.subspan(i + 64, 8), Catch::Matchers::RangeEquals(s1.subspan(2 * i + 8, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 0 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 1 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i + 8, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 2 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i + 16, 8)));
+        CATCH_REQUIRE_THAT(s2.subspan(i + 3 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(i + 24, 8)));
     }
     CATCH_REQUIRE(v2 == convertToWidth(v1, 16, 16, 8));
 }
@@ -102,4 +104,97 @@ CATCH_TEST_CASE("convertToSprites", TEST_SUITE_TAG)
         CATCH_REQUIRE_THAT(s2.subspan(i + 7 * 64, 8), Catch::Matchers::RangeEquals(s1.subspan(2 * i + 24, 8)));
     }
     CATCH_REQUIRE(v2 == convertToTiles(convertToWidth(v1, 32, 16, 16), 16, 32, 8));
+}
+
+CATCH_TEST_CASE("buildUniqueTileMap", TEST_SUITE_TAG)
+{
+    std::vector<Color::XRGB8888> v0;
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 5, 8, false, 8, 8));
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 8, 7, false, 8, 8));
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 8, 8, false, 6, 8));
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 8, 8, false, 8, 4));
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 8, 8, false, 8, 8));
+    v0.resize(64);
+    CATCH_REQUIRE_THROWS(buildUniqueTileMap(v0, 8, 8, false, 6, 5));
+    // clang-format off
+    std::vector<Color::XRGB8888> v1 = {
+        1, 1, 1, 1, 1, 1, 1, 1, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 2, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        //-------------------------+---------------------------
+        1, 1, 1, 1, 1, 1, 1, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 1, 1, 1, 1, 1, 1, 1,
+        //-------------------------+---------------------------
+        1, 1, 1, 1, 1, 1, 1, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 1, /* | */ 1, 1, 1, 1, 1, 1, 1, 1,
+        //-------------------------+---------------------------
+        0, 1, 1, 1, 1, 1, 1, 1, /* | */ 0, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 1, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, /* | */ 1, 0, 0, 0, 0, 0, 0, 0,
+        //-------------------------+---------------------------
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+        0, 0, 0, 0, 0, 0, 0, 0, /* | */ 255, 255, 255, 255, 255, 255, 255, 255,
+    };
+    // clang-format on
+    auto v2 = convertToTiles(v1, 16, 40, 8, 8);
+    auto [i3, v3] = buildUniqueTileMap(v2, 16, 40, false, 8, 8);
+    CATCH_REQUIRE(i3.size() == 10);
+    CATCH_REQUIRE(v3.size() == (9 * 8 * 8));
+    CATCH_REQUIRE(i3 == std::vector<uint16_t>{0, 1, 2, 3, 2, 4, 5, 6, 7, 8});
+    std::span s2(v2);
+    std::span s3(v3);
+    for (std::size_t i = 0; i < 64; i += 8)
+    {
+        CATCH_REQUIRE_THAT(s3.subspan(i + 0 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 0 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 1 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 1 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 2 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 2 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 3 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 3 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 4 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 5 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 5 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 6 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 6 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 7 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 7 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 8 * 64, 8)));
+        CATCH_REQUIRE_THAT(s3.subspan(i + 8 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 9 * 64, 8)));
+    }
+    auto [i4, v4] = buildUniqueTileMap(v2, 16, 40, true, 8, 8);
+    CATCH_REQUIRE(i4.size() == 10);
+    CATCH_REQUIRE(v4.size() == (6 * 8 * 8));
+    CATCH_REQUIRE(i4 == std::vector<uint16_t>{0, 1, (1 << 10), (1 << 11), (1 << 10), (3 << 10), 2, 3, 4, 5});
+    std::span s4(v4);
+    for (std::size_t i = 0; i < 64; i += 8)
+    {
+        CATCH_REQUIRE_THAT(s4.subspan(i + 0 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 0 * 64, 8)));
+        CATCH_REQUIRE_THAT(s4.subspan(i + 1 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 1 * 64, 8)));
+        CATCH_REQUIRE_THAT(s4.subspan(i + 2 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 6 * 64, 8)));
+        CATCH_REQUIRE_THAT(s4.subspan(i + 3 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 7 * 64, 8)));
+        CATCH_REQUIRE_THAT(s4.subspan(i + 4 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 8 * 64, 8)));
+        CATCH_REQUIRE_THAT(s4.subspan(i + 5 * 64, 8), Catch::Matchers::RangeEquals(s2.subspan(i + 9 * 64, 8)));
+    }
 }
