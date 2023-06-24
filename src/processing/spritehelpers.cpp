8dcf26777ab9f49813336775ff7febb23a699696
Bim Overbohm <bim.overbohm@googlemail.com>
2023-06-24 18:52:39 +0200

    Fix tile hash generation

--- src/processing/spritehelpers.cpp
+++ src/processing/spritehelpers.cpp
@@ -51,64 +51,80 @@ auto convertToTiles(const std::vector<Color::XRGB8888> &data, uint32_t width, ui
     return dst;
 }
 
-auto convertToSprites(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>
+auto convertToSprites(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>
 {
     REQUIRE(spriteWidth % 8 == 0 && spriteHeight % 8 == 0, std::runtime_error, "Sprite width and height must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
-    return convertToTiles(convertToWidth(src, width, height, spriteWidth), width, height);
+    return convertToTiles(convertToWidth(data, width, height, spriteWidth), width, height);
 }
 
 // Hash tile block 4 different ways: normal, flipped horizontally, flipped vertically, flipped in both directions
-// Uses modified DJB2 hash functions. Not terribly efficient
-auto hashTileBlock(std::vector<Color::XRGB8888>::const_iterator src, uint32_t columns, uint32_t rows, uint32_t pixelsPerRow) -> std::array<uint32_t, 4>
+auto hashTileBlock(const std::vector<Color::XRGB8888>::const_iterator src, uint32_t columns, uint32_t rows, bool hashFlips) -> std::array<uint32_t, 4>
 {
-    std::array<uint32_t, 4> hash = {15359, 11447, 6991, 5381};
+    static const std::array<uint32_t, 256> primes = {1277, 1889, 1021, 1231, 1069, 239, 1789, 1913, 359, 1163, 151, 1861, 103, 47, 1567, 1693, 509, 1297, 467, 1571, 1613, 1097, 491, 1987, 349, 1787, 907, 967, 1213, 41, 1427, 1171, 109, 619, 499, 199, 1489, 857, 421, 1091, 1217, 1999, 13, 97, 257, 1187, 229, 1291, 761, 547, 1831, 1759, 751, 1327, 53, 43, 11, 1459, 1061, 71, 1637, 887, 293, 1601, 659, 1811, 431, 1973, 1871, 853, 409, 1559, 1721, 571, 1993, 787, 181, 1709, 1549, 557, 773, 653, 941, 983, 1867, 1151, 1483, 233, 107, 1621, 317, 733, 1399, 1511, 1879, 1303, 719, 1933, 997, 1259, 1951, 1033, 661, 977, 1409, 59, 127, 641, 701, 1523, 1901, 449, 1453, 1249, 37, 1753, 1367, 1847, 929, 457, 1949, 1579, 1201, 149, 1997, 877, 1451, 1583, 953, 859, 347, 227, 839, 367, 31, 1907, 1117, 271, 139, 911, 1193, 1627, 1123, 1051, 419, 797, 2, 1873, 283, 397, 1031, 131, 179, 23, 157, 263, 1747, 631, 1381, 379, 1039, 863, 1663, 1103, 1697, 313, 919, 1223, 1741, 1657, 1493, 607, 1087, 709, 1423, 1597, 727, 1321, 523, 569, 7, 1009, 167, 677, 563, 1429, 1619, 991, 1153, 73, 613, 383, 79, 17, 89, 211, 947, 643, 61, 811, 241, 829, 1607, 1931, 463, 1877, 373, 197, 599, 1109, 1783, 1471, 937, 1237, 401, 277, 353, 1319, 281, 389, 743, 1307, 587, 1093, 541, 1049, 83, 67, 163, 769, 1447, 1229, 1129, 193, 1487, 337, 617, 461, 647, 443, 883, 1063, 3, 1499, 1667, 1531, 1669, 251, 101, 823, 173, 439, 757, 1433, 1013, 311};
+    static constexpr uint32_t piStart = 5;
+    std::array<uint32_t, 4> hash = {5381, 5381, 5381, 5381};
     // hash normally
+    uint32_t pi = piStart;
     auto src0 = src;
-    for (int32_t i = 0; i < (columns * rows); i++, ++src0)
-    {
-        hash[0] = hash[0] * (11 + static_cast<uint32_t>(src0->R()));
-        hash[0] = hash[0] * (31 + static_cast<uint32_t>(src0->G()));
-        hash[0] = hash[0] * (53 + static_cast<uint32_t>(src0->B()));
-    }
-    // hash flipped horizontally
     for (int32_t y = 0; y < rows; y++)
     {
-        auto src1 = std::next(src, y * pixelsPerRow + pixelsPerRow - 1);
-        for (int32_t x = 0; x < columns; x++, --src1)
+        for (int32_t x = 0; x < columns; x++, ++src0)
         {
-            hash[1] = hash[1] * (7 + static_cast<uint32_t>(src1->R()));
-            hash[1] = hash[1] * (61 + static_cast<uint32_t>(src1->G()));
-            hash[1] = hash[1] * (43 + static_cast<uint32_t>(src1->B()));
+            auto p = primes[pi++ % (primes.size() - 1)];
+            hash[0] += p * (2 + static_cast<uint32_t>(src0->R()));
+            hash[0] += p * (263 + static_cast<uint32_t>(src0->G()));
+            hash[0] += p * (521 + static_cast<uint32_t>(src0->B()));
         }
     }
-    // hash flipped vertically
-    for (int32_t y = rows - 1; y >= 0; y--)
+    if (hashFlips)
     {
-        auto src2 = std::next(src, y * pixelsPerRow);
-        for (int32_t x = 0; x < columns; x++, ++src2)
+        // hash flipped horizontally
+        pi = piStart;
+        for (int32_t y = 0; y < rows; y++)
         {
-            hash[2] = hash[2] * (47 + static_cast<uint32_t>(src2->R()));
-            hash[2] = hash[2] * (19 + static_cast<uint32_t>(src2->G()));
-            hash[2] = hash[2] * (37 + static_cast<uint32_t>(src2->B()));
+            auto src1 = std::next(src, y * columns + columns - 1);
+            for (int32_t x = columns - 1; x >= 0; x--, --src1)
+            {
+                auto p = primes[pi++ % (primes.size() - 1)];
+                hash[1] += p * (2 + static_cast<uint32_t>(src1->R()));
+                hash[1] += p * (263 + static_cast<uint32_t>(src1->G()));
+                hash[1] += p * (521 + static_cast<uint32_t>(src1->B()));
+            }
         }
-    }
-    // hash flipped both ways
-    for (int32_t y = rows - 1; y >= 0; y--)
-    {
-        auto src3 = std::next(src, y * pixelsPerRow + pixelsPerRow - 1);
-        for (int32_t x = 0; x < columns; x++, --src3)
+        // hash flipped vertically
+        pi = piStart;
+        for (int32_t y = rows - 1; y >= 0; y--)
         {
-            hash[3] = hash[3] * (29 + static_cast<uint32_t>(src3->R()));
-            hash[3] = hash[3] * (17 + static_cast<uint32_t>(src3->G()));
-            hash[3] = hash[3] * (13 + static_cast<uint32_t>(src3->B()));
+            auto src2 = std::next(src, y * columns);
+            for (int32_t x = 0; x < columns; x++, ++src2)
+            {
+                auto p = primes[pi++ % (primes.size() - 1)];
+                hash[2] += p * (2 + static_cast<uint32_t>(src2->R()));
+                hash[2] += p * (263 + static_cast<uint32_t>(src2->G()));
+                hash[2] += p * (521 + static_cast<uint32_t>(src2->B()));
+            }
+        }
+        // hash flipped both ways
+        pi = piStart;
+        auto src3 = std::next(src, (rows - 1) * columns + columns - 1);
+        for (int32_t y = 0; y < rows; y++)
+        {
+            for (int32_t x = columns - 1; x >= 0; x--, --src3)
+            {
+                auto p = primes[pi++ % (primes.size() - 1)];
+                hash[3] += p * (2 + static_cast<uint32_t>(src3->R()));
+                hash[3] += p * (263 + static_cast<uint32_t>(src3->G()));
+                hash[3] += p * (521 + static_cast<uint32_t>(src3->B()));
+            }
         }
     }
     return hash;
 }
 
-auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth, uint32_t tileHeight) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>
+auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth, uint32_t tileHeight) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>
 {
+    REQUIRE(data.size() == width * height, std::runtime_error, "Data size must be == width * height");
     REQUIRE(tileWidth % 8 == 0 && tileHeight % 8 == 0, std::runtime_error, "Tile width and height must be divisible by 8");
     REQUIRE(width % 8 == 0 && height % 8 == 0, std::runtime_error, "Width and height must be divisible by 8");
     const uint32_t pixelsPerTile = tileWidth * tileHeight;
@@ -116,29 +132,26 @@ auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width,
     std::vector<Color::XRGB8888> dstTiles;                                    // unique tile map
     uint16_t nrOfUniqueTiles = 0;                                             // # of tiles currently in tile map
     std::map<uint32_t, uint16_t> dstTileHashes;                               // map from hash values of tile pixels -> tile map index
-    std::map<uint32_t, uint16_t> dstTileHashesH;                              // map from hash values of tile pixels swapped in H -> tile map index
-    std::map<uint32_t, uint16_t> dstTileHashesV;                              // map from hash values of tile pixels swapped in V -> tile map index
-    std::map<uint32_t, uint16_t> dstTileHashesHV;                             // map from hash values of tile pixels swapped in H&V -> tile map index
     // find screen map indices for all tiles while sorting out duplicates
-    auto srcIt = src.cbegin();
+    auto srcIt = data.cbegin();
     for (uint32_t tileIndex = 0; tileIndex < dstScreen.size(); tileIndex++)
     {
         // hash tile pixel block
-        auto tileHash = hashTileBlock(srcIt, tileWidth, tileHeight, width);
+        auto tileHash = hashTileBlock(srcIt, tileWidth, tileHeight, detectFlips);
         // check if tile pixel hash can be found in one of the hash maps
         if (auto tileIt = dstTileHashes.find(tileHash[0]); tileIt != dstTileHashes.cend())
         {
             dstScreen[tileIndex] = tileIt->second;
         }
-        else if (auto tileIt = dstTileHashesH.find(tileHash[1]); detectFlips && tileIt != dstTileHashesH.cend())
+        else if (auto tileIt = dstTileHashes.find(tileHash[1]); detectFlips && tileIt != dstTileHashes.cend())
         {
             dstScreen[tileIndex] = tileIt->second;
         }
-        else if (auto tileIt = dstTileHashesV.find(tileHash[2]); detectFlips && tileIt != dstTileHashesV.cend())
+        else if (auto tileIt = dstTileHashes.find(tileHash[2]); detectFlips && tileIt != dstTileHashes.cend())
         {
             dstScreen[tileIndex] = tileIt->second;
         }
-        else if (auto tileIt = dstTileHashesHV.find(tileHash[3]); detectFlips && tileIt != dstTileHashesHV.cend())
+        else if (auto tileIt = dstTileHashes.find(tileHash[3]); detectFlips && tileIt != dstTileHashes.cend())
         {
             dstScreen[tileIndex] = tileIt->second;
         }
@@ -149,9 +162,12 @@ auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width,
             dstScreen[tileIndex] = nrOfUniqueTiles;
             // add this tiles index to all hash maps
             dstTileHashes[tileHash[0]] = nrOfUniqueTiles;
-            dstTileHashesH[tileHash[1]] = nrOfUniqueTiles | (1 << 10);
-            dstTileHashesV[tileHash[2]] = nrOfUniqueTiles | (1 << 11);
-            dstTileHashesHV[tileHash[3]] = nrOfUniqueTiles | (3 << 10);
+            if (detectFlips)
+            {
+                dstTileHashes[tileHash[1]] = nrOfUniqueTiles | (1 << 10);
+                dstTileHashes[tileHash[2]] = nrOfUniqueTiles | (1 << 11);
+                dstTileHashes[tileHash[3]] = nrOfUniqueTiles | (3 << 10);
+            }
             nrOfUniqueTiles++;
             // copy new tile data to tile map
             std::copy(srcIt, std::next(srcIt, pixelsPerTile), std::back_inserter(dstTiles));
--- src/processing/spritehelpers.h
+++ src/processing/spritehelpers.h
@@ -6,18 +6,22 @@
 #include <vector>
 
 /// @brief Cut data to tileWidth * height pixel wide tiles. Width and height and tileWidth MUST be a multiple of 8!
+/// Moves from left to right first, then top to bottom.
 auto convertToWidth(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth = 8) -> std::vector<Color::XRGB8888>;
 
 /// @brief Cut data to 8x8 pixel wide tiles and store per tile instead of per scanline.
 /// Width and height MUST be a multiple of 8!
+/// Moves from left to right first, then top to bottom.
 auto convertToTiles(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t tileWidth = 8, uint32_t tileHeight = 8) -> std::vector<Color::XRGB8888>;
 
 /// @brief Cut data to 8x8 pixel wide tiles and store per sprite instead of per scanline.
 /// Width and height MUST be a multiple of 8 and of spriteWidth and spriteHeight.
-auto convertToSprites(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>;
+/// Moves from left to right first, then top to bottom
+auto convertToSprites(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, uint32_t spriteWidth, uint32_t spriteHeight) -> std::vector<Color::XRGB8888>;
 
 /// @brief Build a screen and tile map from tile data, storing only unique tiles. Only max. 1024 unique tiles allowed!
 /// Source data MUST have been converted to tiles already and width and height MUST be a multiple of 8!
-/// Will detect horizontally, vertically and horizontally+vertically flipped tiles and will set the map index flip flags accordingly (if detectFlips == true)
+/// Moves from left to right first, then top to bottom.
+/// @param detectFlips Pass true to detect horizontally, vertically and horizontally+vertically flipped tiles and will set the map index flip flags accordingly.
 /// @return Returns (screen map, unique tile map)
-auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &src, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth = 8, uint32_t tileHeight = 8) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>;
+auto buildUniqueTileMap(const std::vector<Color::XRGB8888> &data, uint32_t width, uint32_t height, bool detectFlips, uint32_t tileWidth = 8, uint32_t tileHeight = 8) -> std::pair<std::vector<uint16_t>, std::vector<Color::XRGB8888>>;
