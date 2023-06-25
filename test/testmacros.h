c63d8d4d3b9db4b035002c18addc92759b4cb148
Bim Overbohm <bim.overbohm@googlemail.com>
2023-06-25 13:49:00 +0200

    Document test macros

--- test/testmacros.h
+++ test/testmacros.h
@@ -3,10 +3,13 @@
 #include <catch2/catch_test_macros.hpp>
 #include <string>
 
-#define SCAT_NX(A, B) A##B
-#define SCAT(A, B) SCAT_NX(A, B)
-
+// Define the test suite name. Call before yout first test.
+// It will also define the test prefix name + "::"
+// e.g. TEST_SUITE("Foo") -> suite name "Foo", all tests prefixed with "Foo::"
 #define TEST_SUITE(a)                                           \
     static const inline std::string TEST_SUITE_TAG = "[" a "]"; \
     static const inline std::string TEST_SUITE_PREFIX = a "::";
+
+// Define a Catch2 test case. Will be prefixed with test suite name + "::", e.g.
+// TEST_SUITE("Foo"); TEST_CASE("bar"); -> Test name "Foo::bar"
 #define TEST_CASE(b) CATCH_TEST_CASE(TEST_SUITE_PREFIX + b, TEST_SUITE_TAG)
