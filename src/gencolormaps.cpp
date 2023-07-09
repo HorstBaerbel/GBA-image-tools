2418eddfd727875058704b136dee1cef0dbcd08e
Bim Overbohm <bim.overbohm@googlemail.com>
2023-07-09 15:59:48 +0200

    Rename colormap555 to gencolormaps and generage RGB565 color map too

---
+++ src/gencolormaps.cpp
@@ -0,0 +1,47 @@
+// Generates a map of all 32768 displayable colors in RGB555 color space and as colors grouped by hue
+// You'll need libmagick++-dev installed!
+
+#include "color/colorhelpers.h"
+#include "color/lchf.h"
+#include "io/imageio.h"
+
+int main(int argc, char *argv[])
+{
+    // Generate RGB555 colors
+    std::vector<Color::XRGB8888> colors555;
+    for (uint8_t r = 0; r < 32; ++r)
+    {
+        for (uint8_t g = 0; g < 32; ++g)
+        {
+            for (uint8_t b = 0; b < 32; ++b)
+            {
+                colors555.push_back(Color::XRGB8888((255.0F * r) / 31.0F, (255.0F * g) / 31.0F, (255.0F * b) / 31.0F));
+            }
+        }
+    }
+    Image::ImageData pixels555(colors555);
+    Image::Data data555;
+    data555.fileName = "colormap555.png";
+    data555.size = {256, 128};
+    data555.imageData = pixels555;
+    IO::File::writeImage(".", data555);
+    // Generate RGB565 colors
+    std::vector<Color::XRGB8888> colors565;
+    for (uint8_t r = 0; r < 32; ++r)
+    {
+        for (uint8_t g = 0; g < 64; ++g)
+        {
+            for (uint8_t b = 0; b < 32; ++b)
+            {
+                colors565.push_back(Color::XRGB8888((255.0F * r) / 31.0F, (255.0F * g) / 63.0F, (255.0F * b) / 31.0F));
+            }
+        }
+    }
+    Image::ImageData pixels565(colors565);
+    Image::Data data565;
+    data565.fileName = "colormap565.png";
+    data565.size = {256, 256};
+    data565.imageData = pixels565;
+    IO::File::writeImage(".", data565);
+    return 0;
+}
