e1ed4c8e918b064530b0a3a6d7fe6625cdc34bdd
Bim Overbohm <bim.overbohm@googlemail.com>
2024-04-14 15:16:27 +0200

    Put color helpers into namespace

--- src/color/colorhelpers.cpp
+++ src/color/colorhelpers.cpp
@@ -2,57 +2,75 @@
 
 #include <algorithm>
 
-auto addColorAtIndex0(const std::vector<Color::XRGB8888> &colorMap, const Color::XRGB8888 &color0) -> std::vector<Color::XRGB8888>
+namespace Color
 {
-    std::vector<Color::XRGB8888> tempMap(colorMap.size() + 1, color0);
-    std::copy(colorMap.cbegin(), colorMap.cend(), std::next(tempMap.begin()));
-    return tempMap;
-}
 
-auto buildColorMapRGB555() -> std::vector<Color::XRGB8888>
-{
-    std::vector<Color::XRGB8888> result;
-    for (uint32_t r = 0; r < 32; ++r)
+    auto addColorAtIndex0(const std::vector<Color::XRGB8888> &colorMap, const Color::XRGB8888 &color0) -> std::vector<Color::XRGB8888>
     {
-        const auto cr = static_cast<uint8_t>((255 * r) / 31);
-        for (uint32_t g = 0; g < 32; ++g)
+        std::vector<Color::XRGB8888> tempMap(colorMap.size() + 1, color0);
+        std::copy(colorMap.cbegin(), colorMap.cend(), std::next(tempMap.begin()));
+        return tempMap;
+    }
+
+    auto buildColorMapRGB555() -> std::vector<Color::XRGB8888>
+    {
+        std::vector<Color::XRGB8888> result;
+        for (uint32_t r = 0; r < 32; ++r)
         {
-            const auto cg = static_cast<uint8_t>((255 * g) / 31);
-            for (uint32_t b = 0; b < 32; ++b)
+            const auto cr = static_cast<uint8_t>((255 * r) / 31);
+            for (uint32_t g = 0; g < 32; ++g)
             {
-                const auto cb = static_cast<uint8_t>((255 * b) / 31);
-                result.emplace_back(Color::XRGB8888(cr, cg, cb));
+                const auto cg = static_cast<uint8_t>((255 * g) / 31);
+                for (uint32_t b = 0; b < 32; ++b)
+                {
+                    const auto cb = static_cast<uint8_t>((255 * b) / 31);
+                    result.emplace_back(Color::XRGB8888(cr, cg, cb));
+                }
             }
         }
+        return result;
     }
-    return result;
-}
 
-auto buildColorMapRGB565() -> std::vector<Color::XRGB8888>
-{
-    std::vector<Color::XRGB8888> result;
-    for (uint32_t r = 0; r < 32; ++r)
+    auto buildColorMapRGB565() -> std::vector<Color::XRGB8888>
     {
-        const auto cr = static_cast<uint8_t>((255 * r) / 31);
-        for (uint32_t g = 0; g < 64; ++g)
+        std::vector<Color::XRGB8888> result;
+        for (uint32_t r = 0; r < 32; ++r)
         {
-            const auto cg = static_cast<uint8_t>((255 * g) / 63);
-            for (uint32_t b = 0; b < 32; ++b)
+            const auto cr = static_cast<uint8_t>((255 * r) / 31);
+            for (uint32_t g = 0; g < 64; ++g)
             {
-                const auto cb = static_cast<uint8_t>((255 * b) / 31);
-                result.emplace_back(Color::XRGB8888(cr, cg, cb));
+                const auto cg = static_cast<uint8_t>((255 * g) / 63);
+                for (uint32_t b = 0; b < 32; ++b)
+                {
+                    const auto cb = static_cast<uint8_t>((255 * b) / 31);
+                    result.emplace_back(Color::XRGB8888(cr, cg, cb));
+                }
             }
         }
+        return result;
     }
-    return result;
-}
 
-auto swapColors(const std::vector<Color::XRGB8888> &colors, const std::vector<uint8_t> &newIndices) -> std::vector<Color::XRGB8888>
-{
-    std::vector<Color::XRGB8888> result;
-    for (uint32_t i = 0; i < colors.size(); i++)
+    auto buildColorMapFor(Color::Format format) -> std::vector<Color::XRGB8888>
     {
-        result.push_back(colors.at(newIndices[i]));
+        if (format == Format::XRGB1555)
+        {
+            return buildColorMapRGB555();
+        }
+        else if (format == Format::RGB565)
+        {
+            return buildColorMapRGB565();
+        }
+        return {};
     }
-    return result;
+
+    auto swapColors(const std::vector<Color::XRGB8888> &colors, const std::vector<uint8_t> &newIndices) -> std::vector<Color::XRGB8888>
+    {
+        std::vector<Color::XRGB8888> result;
+        for (uint32_t i = 0; i < colors.size(); i++)
+        {
+            result.push_back(colors.at(newIndices[i]));
+        }
+        return result;
+    }
+
 }
--- src/color/colorhelpers.h
+++ src/color/colorhelpers.h
@@ -1,18 +1,27 @@
 #pragma once
 
+#include "colorformat.h"
 #include "xrgb8888.h"
 
 #include <cstdint>
 #include <vector>
 
-/// @brief Add color to color map and shift all other colors towards the end by one
-auto addColorAtIndex0(const std::vector<Color::XRGB8888> &colorMap, const Color::XRGB8888 &color0) -> std::vector<Color::XRGB8888>;
+namespace Color
+{
 
-/// @brief Build an image with the RGB555 color space the GBA uses
-auto buildColorMapRGB555() -> std::vector<Color::XRGB8888>;
+    /// @brief Add color to color map and shift all other colors towards the end by one
+    auto addColorAtIndex0(const std::vector<Color::XRGB8888> &colorMap, const Color::XRGB8888 &color0) -> std::vector<Color::XRGB8888>;
 
-/// @brief Build an image with the RGB565 color space the NDS or DXT use
-auto buildColorMapRGB565() -> std::vector<Color::XRGB8888>;
+    /// @brief Build a color map with the RGB555 color space the GBA uses
+    auto buildColorMapRGB555() -> std::vector<Color::XRGB8888>;
 
-/// @brief Swap colors in palette according to index table
-auto swapColors(const std::vector<Color::XRGB8888> &colors, const std::vector<uint8_t> &newIndices) -> std::vector<Color::XRGB8888>;
+    /// @brief Build a color map with the RGB565 color space the NDS or DXT use
+    auto buildColorMapRGB565() -> std::vector<Color::XRGB8888>;
+
+    /// @brief Build a color map for color format color space. Only works for XRGB1555 and RGB565. All other formats will return empty color maps
+    auto buildColorMapFor(Color::Format format) -> std::vector<Color::XRGB8888>;
+
+    /// @brief Swap colors in palette according to index table
+    auto swapColors(const std::vector<Color::XRGB8888> &colors, const std::vector<uint8_t> &newIndices) -> std::vector<Color::XRGB8888>;
+
+}
