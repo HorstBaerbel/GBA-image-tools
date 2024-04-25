44fbff459a0007222c27abc2bf281ce7116d1d11
Bim Overbohm <bim.overbohm@googlemail.com>
2024-04-25 22:43:45 +0200

    Correct quantization function docs

--- src/processing/quantization.h
+++ src/processing/quantization.h
@@ -13,19 +13,19 @@ namespace Image
         /// @brief Quantize pixel data using thresholding
         /// @param[in] data Input image data
         /// @param[in] threshold Greyscale quatization threshold in range [0,1]
-        /// @return Returns pixel data quantized and converted to Color::Format::Paletted1
+        /// @return Returns pixel data quantized and converted to Color::Format::Paletted8
         auto quantizeThreshold(const ImageData &data, float threshold) -> ImageData;
 
         /// @brief Quantize pixel data by choosing closest colors from given palette using cluster-fit
         /// @param[in] data Input image data
         /// @param[in] nrOfColors Number of colors to reduce image to
-        /// @return Returns pixel data quantized and converted to Color::Format::Paletted1, Paletted2, Paletted4 or Paletted8
+        /// @return Returns pixel data quantized and converted to Color::Format::Paletted8
         auto quantizeClosest(const ImageData &data, uint32_t nrOfColors, const std::vector<Color::XRGB8888> &colorMap) -> ImageData;
 
         /// @brief Quantize pixel data using Atkison error-diffusion dither and choosing colors from given palette
         /// @param[in] data Input image data
         /// @param[in] nrOfColors Number of colors to reduce image to
-        /// @return Returns pixel data quantized and converted to Color::Format::Paletted1, Paletted2, Paletted4 or Paletted8
+        /// @return Returns pixel data quantized and converted to Color::Format::Paletted8
         auto atkinsonDither(const ImageData &data, uint32_t nrOfColors, const std::vector<Color::XRGB8888> &colorMap) -> ImageData;
 
     }
