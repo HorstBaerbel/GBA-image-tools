8e4f7ec65151e8038294d80d7c4a0530b9018506
Bim Overbohm <bim.overbohm@googlemail.com>
2024-04-25 22:41:53 +0200

    Move data helpers to namespace

--- src/processing/datahelpers.cpp
+++ src/processing/datahelpers.cpp
@@ -1,57 +1,62 @@
 #include "datahelpers.h"
 
-std::vector<uint8_t> interleave(const std::vector<std::vector<uint8_t>> &data, uint32_t bitsPerPixel)
+namespace DataHelpers
 {
-    // make sure all vectors have the same sizes
-    for (const auto &d : data)
+
+    std::vector<uint8_t> interleave(const std::vector<std::vector<uint8_t>> &data, uint32_t bitsPerPixel)
     {
-        REQUIRE(d.size() == data.front().size(), std::runtime_error, "All data sets to be interleaved must have the same size");
-    }
-    std::vector<uint8_t> result;
-    if (bitsPerPixel == 4)
-    {
-        REQUIRE((data.size() & 1) == 0, std::runtime_error, "If interleave bit depth is 4, an even number of data sets must be passed!");
-        for (uint32_t pi = 0; pi < data.front().size(); pi++)
+        // make sure all vectors have the same sizes
+        for (const auto &d : data)
         {
-            // get all low nibbles
-            for (uint32_t di = 0; di < data.size(); di += 2)
+            REQUIRE(d.size() == data.front().size(), std::runtime_error, "All data sets to be interleaved must have the same size");
+        }
+        std::vector<uint8_t> result;
+        if (bitsPerPixel == 4)
+        {
+            REQUIRE((data.size() & 1) == 0, std::runtime_error, "If interleave bit depth is 4, an even number of data sets must be passed!");
+            for (uint32_t pi = 0; pi < data.front().size(); pi++)
             {
-                uint8_t lowNibbles = (data[di][pi] & 0x0F) | ((data[di + 1][pi] & 0x0F) << 4);
-                result.push_back(lowNibbles);
-            }
-            // get all high nibbles
-            for (uint32_t di = 0; di < data.size(); di += 2)
-            {
-                uint8_t highNibbles = ((data[di][pi] & 0xF0) >> 4) | (data[di + 1][pi] & 0xF0);
-                result.push_back(highNibbles);
+                // get all low nibbles
+                for (uint32_t di = 0; di < data.size(); di += 2)
+                {
+                    uint8_t lowNibbles = (data[di][pi] & 0x0F) | ((data[di + 1][pi] & 0x0F) << 4);
+                    result.push_back(lowNibbles);
+                }
+                // get all high nibbles
+                for (uint32_t di = 0; di < data.size(); di += 2)
+                {
+                    uint8_t highNibbles = ((data[di][pi] & 0xF0) >> 4) | (data[di + 1][pi] & 0xF0);
+                    result.push_back(highNibbles);
+                }
             }
         }
-    }
-    else if (bitsPerPixel == 8)
-    {
-        for (uint32_t pi = 0; pi < data.front().size(); pi++)
+        else if (bitsPerPixel == 8)
         {
-            for (const auto &d : data)
+            for (uint32_t pi = 0; pi < data.front().size(); pi++)
             {
-                result.push_back(d[pi]);
+                for (const auto &d : data)
+                {
+                    result.push_back(d[pi]);
+                }
             }
         }
-    }
-    else if (bitsPerPixel == 15 || bitsPerPixel == 16)
-    {
-        REQUIRE((data.front().size() & 1) == 0, std::runtime_error, "If interleave bit depth is 15 or 16, data size must be even!");
-        for (uint32_t pi = 0; pi < data.front().size(); pi += 2)
+        else if (bitsPerPixel == 15 || bitsPerPixel == 16)
         {
-            for (const auto &d : data)
+            REQUIRE((data.front().size() & 1) == 0, std::runtime_error, "If interleave bit depth is 15 or 16, data size must be even!");
+            for (uint32_t pi = 0; pi < data.front().size(); pi += 2)
             {
-                result.push_back(d[pi]);
-                result.push_back(d[pi + 1]);
+                for (const auto &d : data)
+                {
+                    result.push_back(d[pi]);
+                    result.push_back(d[pi + 1]);
+                }
             }
         }
+        else
+        {
+            THROW(std::runtime_error, "Bits per pixel must be 4, 8, 15 or 16!");
+        }
+        return result;
     }
-    else
-    {
-        THROW(std::runtime_error, "Bits per pixel must be 4, 8, 15 or 16!");
-    }
-    return result;
+
 }
--- src/processing/datahelpers.h
+++ src/processing/datahelpers.h
@@ -9,124 +9,129 @@
 
 #include "exception.h"
 
-/// @brief Fill up the vector with values until its size is a multiple of multipleOf.
-template <typename T>
-std::vector<T> fillUpToMultipleOf(const std::vector<T> &data, uint32_t multipleOf, T value = T())
+namespace DataHelpers
 {
-    if (data.empty())
-    {
-        return {};
-    }
-    std::vector<T> result = data;
-    const auto size = result.size();
-    if (size < multipleOf)
-    {
-        result.resize(multipleOf, value);
-    }
-    else if (size % multipleOf != 0)
-    {
-        auto newSize = ((size + multipleOf - 1) / multipleOf) * multipleOf;
-        result.resize(newSize, value);
-    }
-    REQUIRE((result.size() % multipleOf) == 0, std::runtime_error, "Size not filled up to a multiple of " << multipleOf << "!");
-    return result;
-}
 
-/// @brief Concat all arrays and convert data from type T to type R by raw copy.
-/// Use e.g. to convert multiple arrays of uint8_ts to a single uint32_t array.
-template <typename R, typename T>
-std::vector<R> combineTo(const std::vector<std::vector<T>> &data)
-{
-    size_t combinedSize = 0;
-    for (const auto &current : data)
+    /// @brief Fill up the vector with values until its size is a multiple of multipleOf.
+    template <typename T>
+    std::vector<T> fillUpToMultipleOf(const std::vector<T> &data, uint32_t multipleOf, T value = T())
     {
-        REQUIRE((current.size() * sizeof(T)) % sizeof(R) == 0, std::runtime_error, "Size must be a multiple of " << sizeof(R) << "!");
-        combinedSize += (current.size() * sizeof(T)) / sizeof(R);
+        if (data.empty())
+        {
+            return {};
+        }
+        std::vector<T> result = data;
+        const auto size = result.size();
+        if (size < multipleOf)
+        {
+            result.resize(multipleOf, value);
+        }
+        else if (size % multipleOf != 0)
+        {
+            auto newSize = ((size + multipleOf - 1) / multipleOf) * multipleOf;
+            result.resize(newSize, value);
+        }
+        REQUIRE((result.size() % multipleOf) == 0, std::runtime_error, "Size not filled up to a multiple of " << multipleOf << "!");
+        return result;
     }
-    std::vector<R> result(combinedSize);
-    R *dst = result.data();
-    for (const auto &current : data)
+
+    /// @brief Concat all arrays and convert data from type T to type R by raw copy.
+    /// Use e.g. to convert multiple arrays of uint8_ts to a single uint32_t array.
+    template <typename R, typename T>
+    std::vector<R> combineTo(const std::vector<std::vector<T>> &data)
     {
-        std::memcpy(dst, current.data(), current.size() * sizeof(T));
-        dst += (current.size() * sizeof(T)) / sizeof(R);
+        size_t combinedSize = 0;
+        for (const auto &current : data)
+        {
+            REQUIRE((current.size() * sizeof(T)) % sizeof(R) == 0, std::runtime_error, "Size must be a multiple of " << sizeof(R) << "!");
+            combinedSize += (current.size() * sizeof(T)) / sizeof(R);
+        }
+        std::vector<R> result(combinedSize);
+        R *dst = result.data();
+        for (const auto &current : data)
+        {
+            std::memcpy(dst, current.data(), current.size() * sizeof(T));
+            dst += (current.size() * sizeof(T)) / sizeof(R);
+        }
+        return result;
     }
-    return result;
-}
 
-/// @brief Convert data from type T to type R by raw copy.
-/// Use e.g. to convert multiple arrays of uint8_ts to a single uint32_t array.
-template <typename R, typename T>
-std::vector<R> convertTo(const std::vector<T> &data)
-{
-    REQUIRE((data.size() * sizeof(T)) % sizeof(R) == 0, std::runtime_error, "Size must be a multiple of " << sizeof(R) << "!");
-    std::vector<R> result((data.size() * sizeof(T)) / sizeof(R));
-    std::memcpy(result.data(), data.data(), data.size() * sizeof(T));
-    return result;
-}
-
-/// @brief Return the start index of each sub-vector in the outer vector as if all vectors were concatenated.
-template <typename T>
-std::vector<uint32_t> getStartIndices(const std::vector<std::vector<T>> &data)
-{
-    size_t currentIndex = 0;
-    std::vector<uint32_t> result;
-    for (const auto &current : data)
+    /// @brief Convert data from type T to type R by raw copy.
+    /// Use e.g. to convert multiple arrays of uint8_ts to a single uint32_t array.
+    template <typename R, typename T>
+    std::vector<R> convertTo(const std::vector<T> &data)
     {
-        result.push_back(currentIndex);
-        currentIndex += current.size();
+        REQUIRE((data.size() * sizeof(T)) % sizeof(R) == 0, std::runtime_error, "Size must be a multiple of " << sizeof(R) << "!");
+        std::vector<R> result((data.size() * sizeof(T)) / sizeof(R));
+        std::memcpy(result.data(), data.data(), data.size() * sizeof(T));
+        return result;
     }
-    return result;
-}
 
-/// @brief Divide every element in the vector by a certain value.
-template <typename T>
-std::vector<T> divideBy(const std::vector<T> &data, T divideBy)
-{
-    std::vector<T> result;
-    std::transform(data.cbegin(), data.cend(), std::back_inserter(result), [divideBy](auto t)
-                   { return t / divideBy; });
-    return result;
-}
-
-/// @brief Interleave data: A012... + B012... -> A0B0A1B1A2B2...
-/// Interleaving for nibbles is done by getting the lower nibble first, then the higher nibble.
-/// @param bitsPerPixel Bits per pixel contained in data. Only 4, 8, 15, 16 is allowed here! 15 bpp is treated as 16.
-std::vector<uint8_t> interleave(const std::vector<std::vector<uint8_t>> &data, uint32_t bitsPerPixel);
-
-/// @brief Delta-encode data. First value is stored verbatim. All other values are stored as difference to previous value
-template <typename T>
-std::vector<T> deltaEncode(const std::vector<T> &data)
-{
-    std::vector<T> result;
-    std::adjacent_difference(data.cbegin(), data.cend(), std::back_inserter(result));
-    return result;
-}
-
-/// @brief Decode delta-encoded data. First value is stored verbatim. All other values are stored as difference to previous value
-template <typename T>
-std::vector<T> deltaDecode(const std::vector<T> &data)
-{
-    if (data.empty())
+    /// @brief Return the start index of each sub-vector in the outer vector as if all vectors were concatenated.
+    template <typename T>
+    std::vector<uint32_t> getStartIndices(const std::vector<std::vector<T>> &data)
     {
-        return {};
+        size_t currentIndex = 0;
+        std::vector<uint32_t> result;
+        for (const auto &current : data)
+        {
+            result.push_back(currentIndex);
+            currentIndex += current.size();
+        }
+        return result;
     }
-    std::vector<T> result;
-    T previous = 0;
-    for (auto v : data)
-    {
-        auto decoded = previous + v;
-        result.push_back(decoded);
-        previous = decoded;
-    }
-    return result;
-}
 
-/// @brief Prepend value to array
-template <typename T>
-std::vector<uint8_t> prependValue(const std::vector<uint8_t> &data, T value)
-{
-    std::vector<uint8_t> result(data.size() + sizeof(T));
-    *reinterpret_cast<T *>(result.data()) = value;
-    std::copy(data.cbegin(), data.cend(), std::next(result.begin(), sizeof(T)));
-    return result;
+    /// @brief Divide every element in the vector by a certain value.
+    template <typename T>
+    std::vector<T> divideBy(const std::vector<T> &data, T divideBy)
+    {
+        std::vector<T> result;
+        std::transform(data.cbegin(), data.cend(), std::back_inserter(result), [divideBy](auto t)
+                       { return t / divideBy; });
+        return result;
+    }
+
+    /// @brief Interleave data: A012... + B012... -> A0B0A1B1A2B2...
+    /// Interleaving for nibbles is done by getting the lower nibble first, then the higher nibble.
+    /// @param bitsPerPixel Bits per pixel contained in data. Only 4, 8, 15, 16 is allowed here! 15 bpp is treated as 16.
+    std::vector<uint8_t> interleave(const std::vector<std::vector<uint8_t>> &data, uint32_t bitsPerPixel);
+
+    /// @brief Delta-encode data. First value is stored verbatim. All other values are stored as difference to previous value
+    template <typename T>
+    std::vector<T> deltaEncode(const std::vector<T> &data)
+    {
+        std::vector<T> result;
+        std::adjacent_difference(data.cbegin(), data.cend(), std::back_inserter(result));
+        return result;
+    }
+
+    /// @brief Decode delta-encoded data. First value is stored verbatim. All other values are stored as difference to previous value
+    template <typename T>
+    std::vector<T> deltaDecode(const std::vector<T> &data)
+    {
+        if (data.empty())
+        {
+            return {};
+        }
+        std::vector<T> result;
+        T previous = 0;
+        for (auto v : data)
+        {
+            auto decoded = previous + v;
+            result.push_back(decoded);
+            previous = decoded;
+        }
+        return result;
+    }
+
+    /// @brief Prepend value to array
+    template <typename T>
+    std::vector<uint8_t> prependValue(const std::vector<uint8_t> &data, T value)
+    {
+        std::vector<uint8_t> result(data.size() + sizeof(T));
+        *reinterpret_cast<T *>(result.data()) = value;
+        std::copy(data.cbegin(), data.cend(), std::next(result.begin(), sizeof(T)));
+        return result;
+    }
+
 }
