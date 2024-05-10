cc7c4f9b16256599ccca946de728fddfbb3f178b
Bim Overbohm <bim.overbohm@googlemail.com>
2024-05-10 15:16:30 +0200

    Add color distance helper for vectors

--- src/color/distance.h
+++ src/color/distance.h
@@ -1,21 +1,38 @@
 #pragma once
 
+#include "exception.h"
+
 #include <array>
+#include <vector>
 
 namespace Color
 {
+
+    /// @brief Calculate square of distance between colors (scalar product)
+    /// @return Returns average color distance in [0,1]
+    template <typename T>
+    static auto distance(const std::vector<T> &colors0, const std::vector<T> &colors1) -> float
+    {
+        REQUIRE(colors0.size() == colors1.size(), std::runtime_error, "Data size must be the same");
+        double dist = 0.0;
+        for (auto c0It = colors0.cbegin(), c1It = colors1.cbegin(); c0It != colors0.cend() && c1It != colors1.cend(); ++c0It, ++c1It)
+        {
+            dist += T::distance(*c0It, *c1It);
+        }
+        return static_cast<float>(dist / colors0.size());
+    }
 
     /// @brief Calculate square of distance between colors (scalar product)
     /// @return Returns average block color distance in [0,1]
     template <typename T, std::size_t N>
     static auto distance(const std::array<T, N> &colors0, const std::array<T, N> &colors1) -> float
     {
-        float dist = 0.0F;
+        double dist = 0.0;
         for (auto c0It = colors0.cbegin(), c1It = colors1.cbegin(); c0It != colors0.cend() && c1It != colors1.cend(); ++c0It, ++c1It)
         {
             dist += T::distance(*c0It, *c1It);
         }
-        return dist / N;
+        return static_cast<float>(dist / N);
     }
 
     /// @brief Calculate square of distance between colors (scalar product) and if there are are outliers above a threshold
@@ -24,14 +41,14 @@ namespace Color
     static auto distanceBelowThreshold(const std::array<T, N> &colors0, const std::array<T, N> &colors1, float threshold) -> std::pair<bool, float>
     {
         bool belowThreshold = true;
-        float dist = 0.0F;
+        double dist = 0.0;
         for (auto c0It = colors0.cbegin(), c1It = colors1.cbegin(); c0It != colors0.cend() && c1It != colors1.cend(); ++c0It, ++c1It)
         {
             auto colorDist = T::distance(*c0It, *c1It);
             belowThreshold = belowThreshold ? colorDist < threshold : belowThreshold;
             dist += colorDist;
         }
-        return {belowThreshold, dist / N};
+        return {belowThreshold, static_cast<float>(dist / N)};
     }
 
 }
