81ec28b20baa7fcb751a0cdb4a6be491a365d893
Bim Overbohm <bim.overbohm@googlemail.com>
2024-05-11 19:37:01 +0200

    Use PSNR in DXT test

--- test/test_dxtg.cpp
+++ test/test_dxtg.cpp
@@ -13,52 +13,52 @@
 struct TestFile
 {
     std::string fileName;
-    float maximumError555;
-    float maximumError565;
+    float minPsnr555;
+    float minPsnr565;
 };
 
 const std::vector<TestFile> TestFiles = {
-    {"artificial_384x256.png", 0.0007F, 0.0007F},
-    {"BigBuckBunny_282_384x256.png", 0.0005F, 0.0005F},
-    {"BigBuckBunny_361_384x256.png", 0.0011F, 0.001F},
-    {"BigBuckBunny_40_384x256.png", 0.0002F, 0.0002F},
-    {"BigBuckBunny_648_384x256.png", 0.0009F, 0.0008F},
-    {"BigBuckBunny_664_384x256.png", 0.0005F, 0.0004F},
-    {"bridge_256x384.png", 0.001F, 0.001F},
-    {"flower_foveon_384x256.png", 0.0004F, 0.0004F},
-    {"nightshot_iso_100_384x256.png", 0.0005F, 0.0005F},
-    {"squish_384x384.png", 0.0002F, 0.0001F},
-    {"TearsOfSteel_1200_384x256.png", 0.0007F, 0.0007F},
-    {"TearsOfSteel_676_384x256.png", 0.0006F, 0.0006F}};
+    {"artificial_384x256.png", 31.76F, 31.94F},
+    {"BigBuckBunny_282_384x256.png", 33.54F, 33.87F},
+    {"BigBuckBunny_361_384x256.png", 29.84F, 30.01},
+    {"BigBuckBunny_40_384x256.png", 38.71F, 38.98F},
+    {"BigBuckBunny_648_384x256.png", 30.90F, 31.09F},
+    {"BigBuckBunny_664_384x256.png", 33.87F, 34.30F},
+    {"bridge_256x384.png", 30.08F, 30.24F},
+    {"flower_foveon_384x256.png", 34.66F, 35.04F},
+    {"nightshot_iso_100_384x256.png", 33.02F, 33.33F},
+    {"squish_384x384.png", 38.76F, 40.07F},
+    {"TearsOfSteel_1200_384x256.png", 31.75F, 32.03F},
+    {"TearsOfSteel_676_384x256.png", 32.28F, 32.60F}};
 
 /*
 XRGB1555
-artificial_384x256.png, error: 0.00066631
-BigBuckBunny_282_384x256.png, error: 0.00044242
-BigBuckBunny_361_384x256.png, error: 0.0010368
-BigBuckBunny_40_384x256.png, error: 0.00013436
-BigBuckBunny_648_384x256.png, error: 0.00081222
-BigBuckBunny_664_384x256.png, error: 0.00040964
-bridge_256x384.png, error: 0.00098132
-flower_foveon_384x256.png, error: 0.00034173
-nightshot_iso_100_384x256.png, error: 0.00049836
-squish_384x384.png, error: 0.0001328
-TearsOfSteel_1200_384x256.png, error: 0.0006683
-TearsOfSteel_676_384x256.png, error: 0.00059064
+artificial_384x256.png, psnr: 31.76
+BigBuckBunny_282_384x256.png, psnr: 33.54
+BigBuckBunny_361_384x256.png, psnr: 29.84
+BigBuckBunny_40_384x256.png, psnr: 38.72
+BigBuckBunny_648_384x256.png, psnr: 30.9
+BigBuckBunny_664_384x256.png, psnr: 33.88
+bridge_256x384.png, psnr: 30.08
+flower_foveon_384x256.png, psnr: 34.66
+nightshot_iso_100_384x256.png, psnr: 33.02
+squish_384x384.png, psnr: 38.77
+TearsOfSteel_1200_384x256.png, psnr: 31.75
+TearsOfSteel_676_384x256.png, psnr: 32.29
 
 RGB565
-artificial_384x256.png, error: 0.00063889
-BigBuckBunny_282_384x256.png, error: 0.00040929
-BigBuckBunny_361_384x256.png, error: 0.00099681
-BigBuckBunny_40_384x256.png, error: 0.0001264
-BigBuckBunny_648_384x256.png, error: 0.00077632
-BigBuckBunny_664_384x256.png, error: 0.00037149
-bridge_256x384.png, error: 0.00094442
-flower_foveon_384x256.png, error: 0.00031324
-nightshot_iso_100_384x256.png, error: 0.00046435
-squish_384x384.png, error: 9.8242e-05
-TearsOfSteel_1200_384x256.png, error: 0.00062655
-TearsOfSteel_676_384x256.png, error: 0.00054954
+artificial_384x256.png, psnr: 31.95
+BigBuckBunny_282_384x256.png, psnr: 33.88
+BigBuckBunny_361_384x256.png, psnr: 30.01
+BigBuckBunny_40_384x256.png, psnr: 38.98
+BigBuckBunny_648_384x256.png, psnr: 31.1
+BigBuckBunny_664_384x256.png, psnr: 34.3
+bridge_256x384.png, psnr: 30.25
+flower_foveon_384x256.png, psnr: 35.04
+nightshot_iso_100_384x256.png, psnr: 33.33
+squish_384x384.png, psnr: 40.08
+TearsOfSteel_1200_384x256.png, psnr: 32.03
+TearsOfSteel_676_384x256.png, psnr: 32.6
 */
 
 const std::string DataPath = "../../data/images/test/";
@@ -74,12 +74,12 @@ TEST_CASE("EncodeDecode555")
         // IO::File::writeImage(image, "/tmp", "in.png");
         auto compressedData = DXT::encodeDXT(inPixels, image.size.width(), image.size.height(), false);
         auto outPixels = DXT::decodeDXT(compressedData, image.size.width(), image.size.height(), false);
-        auto imageError = Color::mse(inPixels, outPixels);
+        auto psnr = Color::psnr(inPixels, outPixels);
         // auto xrgb8888 = Image::PixelData(outPixels, Color::Format::XRGB8888);
         // image.imageData.pixels() = Image::PixelData(xrgb8888.convertData<Color::RGB888>(), Color::Format::RGB888);
         //  IO::File::writeImage(image, "/tmp/test555", testFile.fileName);
-        std::cout << "DXT-compressed " << testFile.fileName << ", error: " << std::setprecision(5) << imageError << std::endl;
-        CATCH_REQUIRE(imageError < testFile.maximumError555);
+        std::cout << "DXT-compressed " << testFile.fileName << ", psnr: " << std::setprecision(4) << psnr << std::endl;
+        CATCH_REQUIRE(psnr >= testFile.minPsnr555);
     }
 }
 
@@ -92,11 +92,11 @@ TEST_CASE("EncodeDecode565")
         // IO::File::writeImage(image, "/tmp", "in.png");
         auto compressedData = DXT::encodeDXT(inPixels, image.size.width(), image.size.height(), true);
         auto outPixels = DXT::decodeDXT(compressedData, image.size.width(), image.size.height(), true);
-        auto imageError = Color::mse(inPixels, outPixels);
+        auto psnr = Color::psnr(inPixels, outPixels);
         // auto xrgb8888 = Image::PixelData(outPixels, Color::Format::XRGB8888);
         // image.imageData.pixels() = Image::PixelData(xrgb8888.convertData<Color::RGB888>(), Color::Format::RGB888);
         //  IO::File::writeImage(image, "/tmp/test565", testFile.fileName);
-        std::cout << "DXT-compressed " << testFile.fileName << ", error: " << std::setprecision(5) << imageError << std::endl;
-        CATCH_REQUIRE(imageError < testFile.maximumError565);
+        std::cout << "DXT-compressed " << testFile.fileName << ", psnr: " << std::setprecision(4) << psnr << std::endl;
+        CATCH_REQUIRE(psnr >= testFile.minPsnr565);
     }
 }
