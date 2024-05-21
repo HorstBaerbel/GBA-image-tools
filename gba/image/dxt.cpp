7f796011322897fa1372d73d2416d74eac3afae4
Bim Overbohm <bim.overbohm@googlemail.com>
2024-05-21 21:20:51 +0200

    Update link

--- gba/image/dxt.cpp
+++ gba/image/dxt.cpp
@@ -115,7 +115,7 @@ namespace DXT
 
     /// @brief Lookup table for a 5-bit RGB color component (c0 << 5 | c1) that returns (c2 << 16 | c3)
     /// Formula: (round((2.0*floor(x/32)+(x%32))/3.0)) | (round((floor(x/32)+2.0*(x%32))/3.0)<<16), x in [0,32*32]
-    // Generated using: https://horstbaerbel.github.io/jslut/index.html?equation=(round((2.0*floor(x%2F32)%2B(x%2532))%2F3.0))%20%7C%20(round((floor(x%2F32)%2B2.0*(x%2532))%2F3.0)%3C%3C16)&xstart=0&xend=(31%3C%3C5)|31&count=32*32&bitdepth=uint32_t&format=base10
+    // Generated using: https://horstbaerbel.github.io/jslut/index.html?equation=round((floor(x%2F32)%2B(x%2532))%2F2.0)&xstart=0&xend=(31%3C%3C5)|31&count=32*32&bitdepth=uint16_t&format=base10
     IWRAM_DATA ALIGN(4) const uint8_t C2_ModeHalf_5bit[1024] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
                                                                 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
                                                                 11, 11, 12, 12, 13, 13, 14, 14, 15, 15,
