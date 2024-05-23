511464a19956b235b4f059d285dbac656cccdbaf
Bim Overbohm <bim.overbohm@googlemail.com>
2024-05-23 23:59:04 +0200

    Add PSNR method for arrays

--- src/color/psnr.h
+++ src/color/psnr.h
@@ -27,6 +27,23 @@ namespace Color
         return static_cast<float>(10.0 * std::log10(1.0 / mse));
     }
 
+    /// @brief Calculate power signal noise ratio between colors
+    /// @return Returns a value in [0,1]
+    /// @note Perceptual weighting of colors depends on color format used
+    /// See: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+    /// And: https://stackoverflow.com/questions/16264141/power-signal-noise-ratio-psnr-of-colored-jpeg-image
+    template <typename T, std::size_t N>
+    static auto psnr(const std::array<T, N> &colors0, const std::array<T, N> &colors1) -> float
+    {
+        double dist = 0.0;
+        for (auto c0It = colors0.cbegin(), c1It = colors1.cbegin(); c0It != colors0.cend() && c1It != colors1.cend(); ++c0It, ++c1It)
+        {
+            dist += T::mse(*c0It, *c1It);
+        }
+        const auto mse = dist / colors0.size(); // calculate mean squared error
+        return static_cast<float>(10.0 * std::log10(1.0 / mse));
+    }
+
     /// @brief Calculate mean squared error between colors (scalar product)
     /// @return Returns average color distance in [0,1]
     template <typename T>
