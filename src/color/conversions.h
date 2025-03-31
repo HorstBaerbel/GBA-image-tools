04cccc6c2de8177be32af0f4e3e12210a2b7767d
Bim Overbohm <bim.overbohm@googlemail.com>
2025-03-31 23:53:12 +0200

    Add color conversion template from raw data

--- src/color/conversions.h
+++ src/color/conversions.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <array>
+#include "exception.h"
+
 #include <algorithm>
+#include <array>
+#include <cstdint>
+#include <cstring>
 #include <vector>
 
 namespace Color
@@ -18,8 +22,8 @@ namespace Color
     /// @brief Convert one color format to another with clamping
     /// @tparam T_IN Input color type
     /// @tparam T_OUT Output color type
-    /// @param color Input color
-    /// @return Converted color
+    /// @param colors Input colors
+    /// @return Converted colors
     template <typename T_OUT, typename T_IN, std::size_t N>
     static auto convertTo(const std::array<T_IN, N> &colors) -> std::array<T_OUT, N>
     {
@@ -36,8 +40,8 @@ namespace Color
     /// @brief Convert one color format to another with clamping
     /// @tparam T_IN Input color type
     /// @tparam T_OUT Output color type
-    /// @param color Input color
-    /// @return Converted color
+    /// @param colors Input colors
+    /// @return Converted colors
     template <typename T_OUT, typename T_IN>
     static auto convertTo(const std::vector<T_IN> &colors) -> std::vector<T_OUT>
     {
@@ -49,6 +53,25 @@ namespace Color
         std::transform(colors.cbegin(), colors.cend(), result.begin(), [](const auto &c)
                        { return convertTo<T_OUT>(c); });
         return result;
+    }
+
+    /// @brief Convert data in one color format to another with clamping
+    /// @tparam T_IN Input color type
+    /// @tparam T_OUT Output color type
+    /// @param data Input color data
+    /// @return Converted colors
+    template <typename T_OUT, typename T_IN>
+    static auto convertTo(const std::vector<uint8_t> &data) -> std::vector<T_OUT>
+    {
+        // convert raw data to correct input pixel format
+        REQUIRE(data.size() % sizeof(typename T_IN::pixel_type) == 0, std::runtime_error, "Bad input data");
+        std::vector<T_IN> colors(data.size() / sizeof(typename T_IN::pixel_type));
+        std::memcpy(colors.data(), data.data(), data.size());
+        if constexpr (std::is_same<T_IN, T_OUT>::value)
+        {
+            return colors;
+        }
+        return convertTo<T_OUT>(colors);
     }
 
     /// @brief Swap red and blue component in color
