c06d994976519ec4ccfe576f7908d932b4696538
Bim Overbohm <bim.overbohm@googlemail.com>
2025-04-24 21:49:46 +0200

    Move DXT decompression tables to their own file

---
+++ gba/image/dxt_tables.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace DXT
+{
+
+    /// @brief Lookup table for a 5-bit RGB color component (c0 << 5 | c1) that returns (c2 << 16 | c3)
+    /// Formula: (round((2.0*floor(x/32)+(x%32))/3.0)) | (round((floor(x/32)+2.0*(x%32))/3.0)<<16), x in [0,32*32]
+    // Generated using: https://horstbaerbel.github.io/jslut/index.html?equation=(round((2.0*floor(x%2F32)%2B(x%2532))%2F3.0))%20%7C%20(round((floor(x%2F32)%2B2.0*(x%2532))%2F3.0)%3C%3C16)&xstart=0&xend=(31%3C%3C5)|31&count=32*32&bitdepth=uint32_t&format=base10
+    extern const uint32_t C2C3_ModeThird_5bit[1024];
+
+    /// @brief Lookup table for a 5-bit RGB color component (c0 << 5 | c1) that returns (c2 << 16 | c3)
+    /// Formula: (round((2.0*floor(x/32)+(x%32))/3.0)) | (round((floor(x/32)+2.0*(x%32))/3.0)<<16), x in [0,32*32]
+    // Generated using: https://horstbaerbel.github.io/jslut/index.html?equation=round((floor(x%2F32)%2B(x%2532))%2F2.0)&xstart=0&xend=(31%3C%3C5)|31&count=32*32&bitdepth=uint16_t&format=base10
+    extern const uint8_t C2_ModeHalf_5bit[1024];
+
+}
