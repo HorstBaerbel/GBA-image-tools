86ff3fe79977e140b66fe6be36466b50ab96f01d
Bim <HorstBaerbel@users.noreply.github.com>
2025-05-14 19:26:45 +0200

    Update codec_dxtv.cpp
    Revert to auto

--- gba/video/codec_dxtv.cpp
+++ gba/video/codec_dxtv.cpp
@@ -227,11 +227,11 @@ namespace DXTV
             // get motion-compensated block info
             const uint16_t blockInfo = *dataPtr16++;
             const bool fromPrev = blockInfo & BLOCK_FROM_PREV;
-            const uint16_t *srcPtr16 = reinterpret_cast<const uint16_t *>(fromPrev ? prevPtr32 : currPtr32);
+            auto srcPtr16 = reinterpret_cast<const uint16_t *>(fromPrev ? prevPtr32 : currPtr32);
             // convert offsets to signed values
             constexpr int32_t halfRange = (1 << BLOCK_MOTION_BITS) / 2 - 1;
-            const int32_t offsetX = static_cast<int32_t>(blockInfo & BLOCK_MOTION_MASK) - halfRange;
-            const int32_t offsetY = static_cast<int32_t>((blockInfo >> BLOCK_MOTION_Y_SHIFT) & BLOCK_MOTION_MASK) - halfRange;
+            const auto offsetX = static_cast<int32_t>(blockInfo & BLOCK_MOTION_MASK) - halfRange;
+            const auto offsetY = static_cast<int32_t>((blockInfo >> BLOCK_MOTION_Y_SHIFT) & BLOCK_MOTION_MASK) - halfRange;
             // calculate start of block to copy
             srcPtr16 += offsetY * LineStride16 + offsetX;
             // copy pixels to output block
