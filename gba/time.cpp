7c2fcd5a029fbf5374e5bb9870836d014deb42d8
Bim Overbohm <bim.overbohm@googlemail.com>
2025-07-30 23:08:00 +0200

    Improve timing resolution to 1 ms

--- gba/time.cpp
+++ gba/time.cpp
@@ -6,29 +6,30 @@
 #include <gba_timers.h>
 
 // The system clock is 16.78MHz (F=16*1024*1024 Hz), one cycle is thus approx. 59.59ns
-constexpr uint16_t TimerDividerBits = 2; // (0=F/1, 1=F/64, 2=F/256, 3=F/1024)
-constexpr int32_t TimerIncrement = 164;  // 65536/164=400 -> 1/400=2.5ms
+constexpr uint16_t TimerDividerBits = 1;     // (0=F/1, 1=F/64, 2=F/256, 3=F/1024)
+constexpr int32_t TimerReload = 65536 - 256; // 16*1024*1024 / 64 / 256 = 1024 -> 1 / 1024 = 0.9765 ms
+constexpr int32_t TimeIncrement = 64;        // 0.9765 * 65536 =~ 64
 
 namespace Time
 {
 
     IWRAM_DATA static int32_t current = 0; // Time since timer was started
 
-    // Called each timer tick to increase time value
+    // Called each timer tick to increase time value by 0.9765 ms
     IWRAM_FUNC auto timerTick() -> void
     {
-        current += TimerIncrement;
+        current += TimeIncrement;
     }
 
-    auto start() -> void
+    IWRAM_FUNC auto start() -> void
     {
         irqSet(irqMASKS::IRQ_TIMER3, timerTick);
         irqEnable(irqMASKS::IRQ_TIMER3);
-        REG_TM3CNT_L = 65536 - TimerIncrement;
+        REG_TM3CNT_L = TimerReload;
         REG_TM3CNT_H = TIMER_START | TIMER_IRQ | TimerDividerBits;
     }
 
-    auto stop() -> void
+    IWRAM_FUNC auto stop() -> void
     {
         REG_TM3CNT_H = 0;
         irqDisable(irqMASKS::IRQ_TIMER3);
