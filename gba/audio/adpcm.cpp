b6ce280fbaf30c77391af6ef1db748deb27d8b9c
Bim Overbohm <bim.overbohm@googlemail.com>
2025-11-17 17:51:02 +0100

    More work on ASM ADPCM decoding

--- gba/audio/adpcm.cpp
+++ gba/audio/adpcm.cpp
@@ -6,8 +6,8 @@
 
 // #define USE_ASM
 
-#define ADPCM_DITHER
-#define ADPCM_DITHER_SHIFT 24
+// #define ADPCM_DITHER
+// #define ADPCM_DITHER_SHIFT 24
 
 namespace Adpcm
 {
@@ -15,21 +15,18 @@ namespace Adpcm
     template <>
     void IWRAM_FUNC UnCompWrite32bit<8>(const uint32_t *data, uint32_t dataSize, uint32_t *dst)
     {
-#ifdef USE_ASM
-        UnCompWrite32bit_8bit(data, dataSize, dst);
-#else
-        // copy frame header and skip to data
+        // #else
+        //  copy frame header and skip to data
         const Audio::AdpcmFrameHeader frameHeader = Audio::AdpcmFrameHeader::read(data);
-        data += sizeof(Audio::AdpcmFrameHeader) / 4;
+        auto data8 = reinterpret_cast<const uint8_t *>(data + sizeof(Audio::AdpcmFrameHeader) / 4);
         // uncompress 4-bit ADPCM samples. These are stored planar / per channel, e.g. L0 L1 ... R0 R1 ...
         const auto adpcmDataSize = dataSize - sizeof(Audio::AdpcmFrameHeader);
-        auto data8 = reinterpret_cast<const uint8_t *>(data);
         auto dst8 = reinterpret_cast<uint8_t *>(dst);
         const auto adpcmChannelBlockSize = adpcmDataSize / frameHeader.nrOfChannels;
         for (uint32_t channel = 0; channel < frameHeader.nrOfChannels; ++channel)
         {
             // align output buffer to next word boundary
-            dst8 = reinterpret_cast<uint8_t *>((reinterpret_cast<uint32_t>(dst8) + 3) & 0xFFFFFFFC);
+            // dst8 = reinterpret_cast<uint8_t *>((reinterpret_cast<uint32_t>(dst8) + 3) & 0xFFFFFFFC);
             // first sample is stored verbatim in header
             int32_t pcmData = *reinterpret_cast<const int16_t *>(data8);
 #ifdef ADPCM_DITHER
@@ -40,25 +37,27 @@ namespace Adpcm
             pcmData = pcmData > 32767 ? 32767 : pcmData;
 #endif
             *dst8++ = pcmData >> 8;
-            int32_t index = data8[2];
+            int32_t index = *reinterpret_cast<const int16_t *>(data8 + 2);
             data8 += 4;
             uint32_t bytesLeft = adpcmChannelBlockSize - 4;
             while (bytesLeft--)
             {
+                // get two ADPCM nibbles
+                const uint32_t nibbles = *data8;
                 // decode first nibble
                 uint32_t step = ADPCM_StepTable[index];
                 uint32_t delta = step >> 3;
-                if (*data8 & 1)
+                if (nibbles & 1)
                     delta += (step >> 2);
-                if (*data8 & 2)
+                if (nibbles & 2)
                     delta += (step >> 1);
-                if (*data8 & 4)
+                if (nibbles & 4)
                     delta += step;
-                if (*data8 & 8)
+                if (nibbles & 8)
                     pcmData -= delta;
                 else
                     pcmData += delta;
-                index += ADPCM_IndexTable_4bit[*data8 & 0x7];
+                index += ADPCM_IndexTable_4bit[nibbles & 0x7];
                 index = index < 0 ? 0 : index;
                 index = index > 88 ? 88 : index;
 #ifdef ADPCM_DITHER
@@ -66,25 +65,25 @@ namespace Adpcm
                 ADPCM_DitherState[0] = ADPCM_DitherState[1] >> ADPCM_DITHER_SHIFT;
                 ADPCM_DitherState[1] = ((ADPCM_DitherState[1] << 4) - ADPCM_DitherState[1]) ^ 1;
 #endif
-                pcmData = pcmData < -32768 ? -32768 : pcmData;
-                pcmData = pcmData > 32767 ? 32767 : pcmData;
+                // pcmData = pcmData < -32768 ? -32768 : pcmData;
+                // pcmData = pcmData > 32767 ? 32767 : pcmData;
                 *dst8++ = pcmData >> 8;
                 // decode second nibble only if not last sample
                 if (bytesLeft > 0)
                 {
                     step = ADPCM_StepTable[index];
                     delta = step >> 3;
-                    if (*data8 & 0x10)
+                    if (nibbles & 0x10)
                         delta += (step >> 2);
-                    if (*data8 & 0x20)
+                    if (nibbles & 0x20)
                         delta += (step >> 1);
-                    if (*data8 & 0x40)
+                    if (nibbles & 0x40)
                         delta += step;
-                    if (*data8 & 0x80)
+                    if (nibbles & 0x80)
                         pcmData -= delta;
                     else
                         pcmData += delta;
-                    index += ADPCM_IndexTable_4bit[(*data8 >> 4) & 0x7];
+                    index += ADPCM_IndexTable_4bit[(nibbles >> 4) & 0x7];
                     index = index < 0 ? 0 : index;
                     index = index > 88 ? 88 : index;
 #ifdef ADPCM_DITHER
@@ -92,15 +91,16 @@ namespace Adpcm
                     ADPCM_DitherState[0] = ADPCM_DitherState[1] >> ADPCM_DITHER_SHIFT;
                     ADPCM_DitherState[1] = ((ADPCM_DitherState[1] << 4) - ADPCM_DitherState[1]) ^ 1;
 #endif
-                    pcmData = pcmData < -32768 ? -32768 : pcmData;
-                    pcmData = pcmData > 32767 ? 32767 : pcmData;
+                    // pcmData = pcmData < -32768 ? -32768 : pcmData;
+                    // pcmData = pcmData > 32767 ? 32767 : pcmData;
                     *dst8++ = pcmData >> 8;
                 }
                 // advance input data
                 data8++;
             }
         }
-#endif
+        // #endif
+        UnCompWrite32bit_8bit(data, dataSize, dst);
     }
 
     template <>
--- gba/audio/adpcm_asm.s
+++ gba/audio/adpcm_asm.s
@@ -4,8 +4,9 @@
  .global ADPCM_IndexTable_4bit
  .global ADPCM_DitherState
 
- #define ADPCM_DITHER
- #define ADPCM_DITHER_SHIFT 24
+ //#define ADPCM_DITHER
+ //#define ADPCM_DITHER_SHIFT 24
+ //#define ADPCM_CLAMP
 
  .arm
  .align
@@ -21,60 +22,60 @@ UnCompWrite32bit_8bit:
     @ r0: pointer to ADPCM frame data, must be 4-byte-aligned (trashed)
     @ r1: size of ADPCM frame data (trashed)
     @ r2: pointer to the 8bit sample buffer, must be 4-byte-aligned (trashed)
-    stmfd sp!, {r4 - r12}
+    @ r3 trashed, r4-r12 and r14 used and saved / restored
+    push {r4 - r12, r14}
     ldr r9, =#0x7FFFFFFF @ used for clamping
     sub r1, r1, #ADPCM_FRAMEHEADER_SIZE @ calculate ADPCM data size
-    ldrh r3, [r0], #ADPCM_FRAMEHEADER_SIZE @ load first header half-word header
-    and r3, r3, #0x60 @ get number of channels into r3
-    mov r3, r3, lsr #5
-    cmp r3, #1
-    ble .adpcm_ucw32_8_mono
-    lsr r1, r1, #1 @ divide size of data by two for stereo
-.adpcm_ucw32_8_mono:
-    sub r12, r1, #4 @ r12 stores ADPCM sample count per channel
+    ldrh r14, [r0], #ADPCM_FRAMEHEADER_SIZE @ load first header half-word and advance past header
+    and r14, r14, #0x60 @ get number of channels into r14
+    mov r14, r14, lsr #5
+    cmp r14, #1
+    movgt r1, r1, lsr #1 @ divide size of data by two for stereo data
 .adpcm_ucw32_8_channel_loop:
-    ldrh r3, [r0], #2 @ load first verbatim PCM sample to r3
+    ldrsh r3, [r0], #2 @ load first verbatim PCM sample to r3
 #ifdef ADPCM_DITHER
     @ dither PCM data in r3
     ldr r6, =ADPCM_DitherState
     ldmia r6, {r7, r8} @ load r7 = last_dither, r8 = dither
-    subs r3, r3, r7 @ pcmData -= last_dither
+    sub r3, r3, r7 @ pcmData -= last_dither
     mov r7, r8 @ r7 = dither
-    rsbs r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
+    rsb r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
     eor r8, r8, #1 @ r8 ^= 1
     mov r7, r7, lsr #ADPCM_DITHER_SHIFT @ r7 = dither >> ADPCM_DITHER_SHIFT
-    adds r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
+    add r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
     sub r6, r6, #8 @ move back to start of ADPCM_dither
     stmia r6, {r7, r8} @ store r7 = new last_dither, r8 = new dither
 #endif
+#ifdef ADPCM_CLAMP
     @ clamp PCM data in r3 to [-32768, 32767]
     mov r8, r3, lsl #16 @ r8 = r3 << 16
     cmp r3, r8, asr #16 @ shift back and sign-extend r8 and compare with r3. check if r3 fits into signed 16-bit
-    eorne r8, r9, r3, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+    eorne r8, r3, r9, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+#endif
     mov r8, r3, asr #8 @ r8 = pcmData >> 8
     strb r8, [r2], #1 @ store first 8-bit PCM sample
+    ldrsh r4, [r0], #2 @ load first index into r4
     ldr r10, =ADPCM_StepTable
     ldr r11, =ADPCM_IndexTable_4bit
-    ldrb r4, [r0], #2 @ load first index into r4
+    sub r12, r1, #4 @ r12 stores ADPCM byte count per channel for loop (-4 as we have already read 4 bytes)
 .adpcm_ucw32_8_sample_loop:
     ldrb r5, [r0], #1 @ load two ADPCM nibbles to r5
-    ldrh r6, [r10, r4] @ load step to r6
-    mov r7, r6, lsr #3 @ load delta to r7
-    ands r8, r5, #0x01 @ ADPCM value & 1?
-    addne r7, r7, r6, lsr #2
-    ands r8, r5, #0x02 @ ADPCM value & 2?
-    addne r7, r7, r6, lsr #1
-    ands r8, r5, #0x04 @ ADPCM value & 4?
+    ldrsh r6, [r10, r4] @ load step to r6
+    mov r7, r6, asr #3 @ load delta to r7
+    tst r5, #0x01 @ ADPCM value & 1?
+    addne r7, r7, r6, asr #2
+    tst r5, #0x02 @ ADPCM value & 2?
+    addne r7, r7, r6, asr #1
+    tst r5, #0x04 @ ADPCM value & 4?
     addne r7, r7, r6
-    ands r8, r5, #0x08 @ ADPCM value & 8?
+    tst r5, #0x08 @ ADPCM value & 8?
     subne r3, r7
     addeq r3, r7
-    and r6, r5, #7
-    ldrh r7, [r11, r6]
-    add r4, r4, r7
+    and r6, r5, #0x07
+    ldrsh r7, [r11, r6] @ load index into r7 and 
+    adds r4, r4, r7 @ add to old index in r4
     @ clamp index in r4 to [0, 88]
-    cmp r4, #0
-    movlt r4, #0
+    movmi r4, #0
     cmp r4, #88
     movgt r4, #88
 #ifdef ADPCM_DITHER
@@ -83,67 +84,73 @@ UnCompWrite32bit_8bit:
     ldmia r6, {r7, r8} @ load r7 = last_dither, r8 = dither
     subs r3, r3, r7 @ pcmData -= last_dither
     mov r7, r8 @ r7 = dither
-    rsbs r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
+    rsb r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
     eor r8, r8, #1 @ r8 ^= 1
     mov r7, r7, lsr #ADPCM_DITHER_SHIFT @ r7 = dither >> ADPCM_DITHER_SHIFT
-    adds r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
+    add r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
     sub r6, r6, #8 @ move back to start of ADPCM_dither
     stmia r6, {r7, r8} @ store r7 = new last_dither, r8 = new dither
 #endif
+#ifdef ADPCM_CLAMP
     @ clamp PCM data in r3 to [-32768, 32767]
     mov r8, r3, lsl #16 @ r8 = r3 << 16
     cmp r3, r8, asr #16 @ shift back and sign-extend r8 and compare with r3. check if r3 fits into signed 16-bit
-    eorne r8, r9, r3, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+    eorne r8, r3, r9, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+#endif
     mov r8, r3, asr #8 @ r8 = pcmData >> 8
     strb r8, [r2], #1 @ store first nibble / 8-bit PCM sample
     @ if not last sample
     cmp r12, #0
-    beq .adpcm_ucw32_8_end
+    beq .adpcm_ucw32_8_sample_end
     @ decode second nibble
-    ldrh r6, [r10, r4] @ load step to r6
-    mov r7, r6, lsr #3 @ load delta to r7
-    ands r8, r5, #0x10 @ ADPCM value & 0x10?
-    addne r7, r7, r6, lsr #2
-    ands r8, r5, #0x20 @ ADPCM value & 0x20?
-    addne r7, r7, r6, lsr #1
-    ands r8, r5, #0x40 @ ADPCM value & 0x40?
+    ldrsh r6, [r10, r4] @ load step to r6
+    mov r7, r6, asr #3 @ load delta to r7
+    tst r5, #0x10 @ ADPCM value & 0x10?
+    addne r7, r7, r6, asr #2
+    tst r5, #0x20 @ ADPCM value & 0x20?
+    addne r7, r7, r6, asr #1
+    tst r5, #0x40 @ ADPCM value & 0x40?
     addne r7, r7, r6
-    ands r8, r5, #0x80 @ ADPCM value & 0x80?
+    tst r5, #0x80 @ ADPCM value & 0x80?
     subne r3, r7
     addeq r3, r7
-    and r6, r5, #7
-    mov r6, r6, lsr #4
-    ldrh r7, [r11, r6]
-    add r4, r4, r7
+    mov r6, r5, lsr #4
+    and r6, r6, #0x7
+    ldrsh r7, [r11, r6]
+    adds r4, r4, r7
     @ clamp index in r4 to [0, 88]
-    cmp r4, #0
-    movlt r4, #0
+    movmi r4, #0
     cmp r4, #88
     movgt r4, #88
 #ifdef ADPCM_DITHER
     @ dither PCM data in r3
     ldr r6, =ADPCM_DitherState
     ldmia r6, {r7, r8} @ load r7 = last_dither, r8 = dither
-    subs r3, r3, r7 @ pcmData -= last_dither
+    sub r3, r3, r7 @ pcmData -= last_dither
     mov r7, r8 @ r7 = dither
-    rsbs r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
+    rsb r8, r7, r8, lsl #4 @ r8 = (dither << 4) - dither
     eor r8, r8, #1 @ r8 ^= 1
     mov r7, r7, lsr #ADPCM_DITHER_SHIFT @ r7 = dither >> ADPCM_DITHER_SHIFT
-    adds r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
+    add r3, r3, r7 @ pcmData += dither >> ADPCM_DITHER_SHIFT
     sub r6, r6, #8 @ move back to start of ADPCM_dither
     stmia r6, {r7, r8} @ store r7 = new last_dither, r8 = new dither
 #endif
+#ifdef ADPCM_CLAMP
     @ clamp PCM data in r3 to [-32768, 32767]
     mov r8, r3, lsl #16 @ r8 = r3 << 16
     cmp r3, r8, asr #16 @ shift back and sign-extend r8 and compare with r3. check if r3 fits into signed 16-bit
-    eorne r8, r9, r3, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+    eorne r8, r3, r9, asr #31 @ extract sign bit of r3. xor with r9 and apply to saturate
+#endif
     mov r8, r3, asr #8 @ r8 = pcmData >> 8
     strb r8, [r2], #1 @ store second nibble / 8-bit PCM sample
     @ decrease number of ADPCM bytes left. if not last byte, loop again
     subs r12, #1
-    bgt .adpcm_ucw32_8_sample_loop
-.adpcm_ucw32_8_end:
-    ldmfd sp!, {r4 - r12}
+    bpl .adpcm_ucw32_8_sample_loop
+.adpcm_ucw32_8_sample_end:
+    @ decrease number of channels left. if not last channel, run sample loop again
+    subs r14, #1
+    bne .adpcm_ucw32_8_channel_loop
+    pop {r4 - r12, r14}
     bx lr
 
  .arm
