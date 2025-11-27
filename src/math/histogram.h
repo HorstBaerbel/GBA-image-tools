c11b2a72c6e8e11316ccf740e97f7173b073d47e
Bim Overbohm <bim.overbohm@googlemail.com>
2025-11-27 16:32:47 +0100

    Add function to normalize histograms

--- src/math/histogram.h
+++ src/math/histogram.h
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <map>
+#include <numeric>
 #include <vector>
 
 namespace Histogram
@@ -14,6 +15,16 @@ namespace Histogram
         std::for_each(data.cbegin(), data.cend(), [&histogram](auto value)
                       { histogram[value]++; });
         return histogram;
+    }
+
+    template <typename T>
+    auto normalizeHistogram(const std::map<T, uint64_t> &histogram) -> std::map<T, float>
+    {
+        std::map<T, float> normalized;
+        const uint64_t sum = std::accumulate(histogram.cbegin(), histogram.cend(), uint64_t(0));
+        std::transform(histogram.cbegin(), histogram.cend(), std::inserter(normalized, normalized.end()), [sum](const auto &entry)
+                       { return {entry.first, static_cast<float>(static_cast<double>(entry.second) / sum)}; });
+        return normalized;
     }
 
 }
