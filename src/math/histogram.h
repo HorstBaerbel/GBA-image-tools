cfcc0e0292722c503404db9a9579bdd05276c2c7
Bim Overbohm <bim.overbohm@googlemail.com>
2025-12-03 14:44:58 +0100

    Make histogram template types a bit more flexible

--- src/math/histogram.h
+++ src/math/histogram.h
@@ -10,19 +10,19 @@
 namespace Histogram
 {
 
-    template <typename T>
-    auto buildHistogram(const std::vector<T> &data) -> std::map<T, uint64_t>
+    template <typename T, typename F = uint64_t>
+    auto buildHistogram(const std::vector<T> &data) -> std::map<T, F>
     {
-        std::map<T, uint64_t> histogram;
+        std::map<T, F> histogram;
         std::for_each(data.cbegin(), data.cend(), [&histogram](auto value)
                       { histogram[value]++; });
         return histogram;
     }
 
-    template <typename T>
-    auto buildHistogramKeepEmpty(const std::vector<T> &data) -> std::map<T, uint64_t>
+    template <typename T, typename F = uint64_t>
+    auto buildHistogramKeepEmpty(const std::vector<T> &data) -> std::map<T, F>
     {
-        std::map<T, uint64_t> histogram;
+        std::map<T, F> histogram;
         for (std::size_t i = 0; i <= std::numeric_limits<T>::max(); ++i)
         {
             histogram[i] = 0;
@@ -32,12 +32,12 @@ namespace Histogram
         return histogram;
     }
 
-    template <typename T>
-    auto normalizeHistogram(const std::map<T, uint64_t> &histogram) -> std::map<T, float>
+    template <typename T, typename F = uint64_t>
+    auto normalizeHistogram(const std::map<T, F> &histogram) -> std::map<T, float>
     {
         std::map<T, float> normalized;
-        const uint64_t sum = std::accumulate(histogram.cbegin(), histogram.cend(), uint64_t(0), [](const auto &a, const auto &b)
-                                             { return a + b.second; });
+        const F sum = std::accumulate(histogram.cbegin(), histogram.cend(), F(0), [](const auto &a, const auto &b)
+                                      { return a + b.second; });
         std::transform(histogram.cbegin(), histogram.cend(), std::inserter(normalized, normalized.end()), [sum](const auto &entry)
                        { return std::make_pair(entry.first, static_cast<float>(static_cast<double>(entry.second) / sum)); });
         return normalized;
