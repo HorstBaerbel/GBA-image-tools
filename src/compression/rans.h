56685c7f6b105d1dd29adf42c9ec6d1972c1686f
Bim Overbohm <bim.overbohm@googlemail.com>
2025-12-05 16:02:27 +0100

    Clarify rANS in header

--- src/compression/rans.h
+++ src/compression/rans.h
@@ -17,19 +17,19 @@ namespace Compression
     static constexpr uint8_t RANS_HEADER_MODE_MASK = 3 << RANS_HEADER_MIN_BITS;   // Bits attributed to header mode
     static constexpr uint8_t RANS_HEADER_MODE_SINGLE = 0 << RANS_HEADER_MIN_BITS; // Flag value for single-symbol mode
     static constexpr uint8_t RANS_HEADER_MODE_256 = 1 << RANS_HEADER_MIN_BITS;    // Flag value for 256 count mode
-    static constexpr uint8_t RANS_HEADER_MODE_RLE = 2 << RANS_HEADER_MIN_BITS;    // Flag value for RLE mode
+    static constexpr uint8_t RANS_HEADER_MODE_RLE = 2 << RANS_HEADER_MIN_BITS;    // Flag value for RLE mode (not implemented)
 
-    /// @brief Compress input data using rANS with a 256-byte alphabet and TOTFREQ = 256.
+    /// @brief Compress input data using rANS with a 256-byte alphabet and RANS_M = 256.
     /// Stream format:
-    ///   0: 3 bytes uncompressed size
-    ///   3: 1 byte rANS type marker "0x40"
-    ///   4: 1 byte rANS header mode
-    ///   5: 256 bytes symbol frequencies
-    /// 261: 1 byte initial rANS state size
-    /// 262: 0-4 byte initial rANS state
-    /// 266: N compressed data
+    ///       0: 3 bytes uncompressed size
+    ///       3: 1 byte rANS type marker "0x40"
+    ///       4: 1 byte rANS header mode
+    ///       5: C bytes symbol frequencies (256 or 512 bytes)
+    ///     5+C: 1 byte initial rANS state size S
+    ///   5+C+1: 0-4 byte initial rANS state
+    /// 5+C+1+S: N compressed data
     auto encodeRANS(const std::vector<uint8_t> &data) -> std::vector<uint8_t>;
 
-    /// @brief Decompress input data using rANS with a 256-byte alphabet and TOTFREQ = 256
+    /// @brief Decompress input data using rANS with a 256-byte alphabet and RANS_M = 256
     auto decodeRANS(const std::vector<uint8_t> &data) -> std::vector<uint8_t>;
 }
