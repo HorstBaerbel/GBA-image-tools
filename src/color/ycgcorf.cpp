df9b2fe3459b2d9c3bbce09a2b459b036b378b6d
Bim Overbohm <bim.overbohm@googlemail.com>
2026-01-06 22:24:07 +0100

    Tweak YCgCoRf MSE calculation

--- src/color/ycgcorf.cpp
+++ src/color/ycgcorf.cpp
@@ -20,6 +20,6 @@ namespace Color
         auto dY = color0.Y() - color1.Y();                                    // [-1,1]
         auto dCg = 0.5F * (color0.Cg() + 1.0F) - 0.5F * (color1.Cg() + 1.0F); // [-1,1]
         auto dCo = 0.5F * (color0.Co() + 1.0F) - 0.5F * (color1.Co() + 1.0F); // [-1,1]
-        return (0.5F * dY * dY + 0.25F * dCg * dCg + 0.25F * dCo * dCo);
-    } // max:  (0.5  *  1 *  1 + 0.25  *   1 *   1 + 0.25  *   1 *   1) = 1
+        return (0.4F * dY * dY + 0.3F * dCg * dCg + 0.3F * dCo * dCo);
+    } // max:  (0.4  *  1 *  1 + 0.3  *   1 *   1 + 0.3  *   1 *   1) = 1
 }
