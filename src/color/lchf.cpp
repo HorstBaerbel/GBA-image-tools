8264e7baf72d6fd26584e029076b8365fd82a5fa
Bim Overbohm <bim.overbohm@googlemail.com>
2026-01-06 22:23:41 +0100

    Improve makeshift LChf MSE calculation

--- src/color/lchf.cpp
+++ src/color/lchf.cpp
@@ -13,12 +13,12 @@ namespace Color
         {
             return 0.0F;
         }
-        float dL = color0.L() - color1.L(); // [-1,1]
-        float dC = color0.C() - color1.C(); // [-1,1]
+        float dL = (color0.L() - color1.L()) / 100.0F; // [-1,1]
+        float dC = (color0.C() - color1.C()) / 200.0F; // [-1,1]
         // use closest hue distance to make hue wrap around
-        float dH0 = 2.0F * std::abs((color0.h() * OneOver360) - (color1.h() * OneOver360));
-        float dH1 = 2.0F - dH0;
-        float dH = dH0 < dH1 ? dH0 : dH1; // [0, 1]
-        return (0.5F * dL * dL + 0.3F * dC * dC + 0.2F * dH * dH) / 3.0F;
-    } // max:  (0.5  *  1 *  1 + 0.3  *  1 *  1 + 0.2  *  1 *  1) / 3 = 1
+        float dH0 = std::abs((color0.h() * OneOver360) - (color1.h() * OneOver360)); // [0,1]
+        float dH1 = 1.0F - dH0;
+        float dH = dH0 < dH1 ? dH0 : dH1; // [0,1]
+        return (0.5F * dL * dL + 0.3F * dC * dC + 0.2F * dH * dH);
+    } // max:  (0.5  *  1 *  1 + 0.3  *  1 *  1 + 0.2  *  1 *  1) = 1
 }
